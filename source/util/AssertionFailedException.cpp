bc8e310beee84e566f4deb4276e959b9ea036b1e
Sandro Santilli <strk@keybit.net>
2003-10-20 13:56:34 +0000

    fixed typo
    git-svn-id: http://svn.osgeo.org/geos/trunk@213 5242fede-7e19-0410-aef8-94bd7d2200fb

--- source/util/AssertionFailedException.cpp
+++ source/util/AssertionFailedException.cpp
@@ -1,13 +1,19 @@
+/*
+* $Log$
+* Revision 1.6  2003/10/20 13:56:34  strk
+* fixed typo
+*
+*/
 #include "../headers/util.h"
 
 namespace geos {
 
 AssertionFailedException::AssertionFailedException(){
 	GEOSException();
-	setName("AssertionFaiedException");
+	setName("AssertionFailedException");
 }
 AssertionFailedException::AssertionFailedException(string msg){
-	setName("AssertionFaiedException");
+	setName("AssertionFailedException");
 	setMessage(msg);
 }
 
