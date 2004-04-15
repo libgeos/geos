99dbe037e9081777ba9d4a300fae181f57a70e04
Sandro Santilli <strk@keybit.net>
2004-04-15 15:11:38 +0000

    Commented out deletion that seems to cause segfaults
    git-svn-id: http://svn.osgeo.org/geos/trunk@310 5242fede-7e19-0410-aef8-94bd7d2200fb

--- source/io/Unload.cpp
+++ source/io/Unload.cpp
@@ -13,6 +13,9 @@
  *
  **********************************************************************
  * $Log$
+ * Revision 1.7  2004/04/15 15:11:38  strk
+ * Commented out deletion that seems to cause segfaults
+ *
  * Revision 1.6  2004/04/15 14:00:31  strk
  * Added new cleanup to Unload::Release
  *
@@ -63,11 +66,11 @@ void Unload::Release()
 	delete RelateComputer::li;
 	delete RelateComputer::ptLocator;
 	delete QuadTreeRoot::origin;
-	delete PrecisionModel::FIXED;
-	delete PrecisionModel::FLOATING;
-	delete PrecisionModel::FLOATING_SINGLE;
+	//delete PrecisionModel::FIXED;
+	//delete PrecisionModel::FLOATING;
+	//delete PrecisionModel::FLOATING_SINGLE;
 	delete PrecisionModel::Type::nameToTypeMap;
-	delete Geometry::geometryChangedFilter;
+	//delete Geometry::geometryChangedFilter;
 	delete Geometry::INTERNAL_GEOMETRY_FACTORY;
 	delete BufferBuilder::cga;
 }
