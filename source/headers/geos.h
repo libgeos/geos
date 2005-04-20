ec24cc89b6ad8851424434e21c801d2892546615
Sandro Santilli <strk@keybit.net>
2005-04-20 17:18:31 +0000

    Added standard copyright header
    git-svn-id: http://svn.osgeo.org/geos/trunk@692 5242fede-7e19-0410-aef8-94bd7d2200fb

--- source/headers/geos.h
+++ source/headers/geos.h
@@ -1,3 +1,18 @@
+/**********************************************************************
+ * $Id$
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.refractions.net
+ *
+ * Copyright (C) 2001-2002 Vivid Solutions Inc.
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation. 
+ * See the COPYING file for more information.
+ *
+ **********************************************************************/
+
 #ifndef GEOS_H
 #define GEOS_H
 
