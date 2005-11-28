0cf8a80485e659bd9aef24bd4ab28856de951e8f
Sandro Santilli <strk@keybit.net>
2005-11-28 18:37:32 +0000

    Minor warning removal
    git-svn-id: http://svn.osgeo.org/geos/trunk@927 5242fede-7e19-0410-aef8-94bd7d2200fb

--- source/geom/LineString.cpp
+++ source/geom/LineString.cpp
@@ -11,127 +11,7 @@
  * by the Free Software Foundation. 
  * See the COPYING file for more information.
  *
- **********************************************************************
- * $Log$
- * Revision 1.51  2005/11/24 23:09:15  strk
- * CoordinateSequence indexes switched from int to the more
- * the correct unsigned int. Optimizations here and there
- * to avoid calling getSize() in loops.
- * Update of all callers is not complete yet.
- *
- * Revision 1.50  2005/11/15 10:02:27  strk
- * optimized envelope computation reducing virtual calls
- *
- * Revision 1.49  2005/11/10 09:33:17  strk
- * Removed virtual overloading LineString::compareTo(LineString *)
- *
- * Revision 1.48  2005/06/23 14:22:33  strk
- * Inlined and added missing ::clone() for Geometry subclasses
- *
- * Revision 1.47  2005/02/22 17:10:47  strk
- * Reduced CoordinateSequence::getSize() calls.
- *
- * Revision 1.46  2004/12/03 22:52:56  strk
- * enforced const return of CoordinateSequence::toVector() method to derivate classes.
- *
- * Revision 1.45  2004/11/23 16:22:49  strk
- * Added ElevationMatrix class and components to do post-processing draping of overlayed geometries.
- *
- * Revision 1.44  2004/09/13 09:07:28  strk
- * Ported fix in LineString::isCoordinate
- *
- * Revision 1.43  2004/09/12 03:51:27  pramsey
- * Casting changes to allow OS/X compilation.
- *
- * Revision 1.42  2004/07/22 08:45:50  strk
- * Documentation updates, memory leaks fixed.
- *
- * Revision 1.41  2004/07/19 13:19:30  strk
- * Documentation fixes
- *
- * Revision 1.40  2004/07/08 19:34:49  strk
- * Mirrored JTS interface of CoordinateSequence, factory and
- * default implementations.
- * Added DefaultCoordinateSequenceFactory::instance() function.
- *
- * Revision 1.39  2004/07/06 17:58:22  strk
- * Removed deprecated Geometry constructors based on PrecisionModel and
- * SRID specification. Removed SimpleGeometryPrecisionReducer capability
- * of changing Geometry's factory. Reverted Geometry::factory member
- * to be a reference to external factory.
- *
- * Revision 1.38  2004/07/05 14:23:03  strk
- * More documentation cleanups.
- *
- * Revision 1.37  2004/07/05 10:50:20  strk
- * deep-dopy construction taken out of Geometry and implemented only
- * in GeometryFactory.
- * Deep-copy geometry construction takes care of cleaning up copies
- * on exception.
- * Implemented clone() method for CoordinateSequence
- * Changed createMultiPoint(CoordinateSequence) signature to reflect
- * copy semantic (by-ref instead of by-pointer).
- * Cleaned up documentation.
- *
- * Revision 1.36  2004/07/03 12:51:37  strk
- * Documentation cleanups for DoxyGen.
- *
- * Revision 1.35  2004/07/02 13:28:26  strk
- * Fixed all #include lines to reflect headers layout change.
- * Added client application build tips in README.
- *
- * Revision 1.34  2004/07/01 14:12:44  strk
- *
- * Geometry constructors come now in two flavors:
- * 	- deep-copy args (pass-by-reference)
- * 	- take-ownership of args (pass-by-pointer)
- * Same functionality is available through GeometryFactory,
- * including buildGeometry().
- *
- * Revision 1.33  2004/06/28 21:11:43  strk
- * Moved getGeometryTypeId() definitions from geom.h to each geometry module.
- * Added holes argument check in Polygon.cpp.
- *
- * Revision 1.32  2004/06/15 20:38:44  strk
- * updated to respect deep-copy GeometryCollection interface
- *
- * Revision 1.31  2004/05/07 09:05:13  strk
- * Some const correctness added. Fixed bug in GeometryFactory::createMultiPoint
- * to handle NULL CoordinateSequence.
- *
- * Revision 1.30  2004/04/20 13:24:15  strk
- * More leaks removed.
- *
- * Revision 1.29  2004/04/20 08:52:01  strk
- * GeometryFactory and Geometry const correctness.
- * Memory leaks removed from SimpleGeometryPrecisionReducer
- * and GeometryFactory.
- *
- * Revision 1.28  2004/04/10 22:41:24  ybychkov
- * "precision" upgraded to JTS 1.4
- *
- * Revision 1.27  2004/04/01 10:44:33  ybychkov
- * All "geom" classes from JTS 1.3 upgraded to JTS 1.4
- *
- * Revision 1.26  2004/03/31 07:50:37  ybychkov
- * "geom" partially upgraded to JTS 1.4
- *
- * Revision 1.25  2003/11/07 01:23:42  pramsey
- * Add standard CVS headers licence notices and copyrights to all cpp and h
- * files.
- *
- * Revision 1.24  2003/10/31 16:36:04  strk
- * Re-introduced clone() method. Copy constructor could not really replace it.
- *
- * Revision 1.23  2003/10/16 08:50:00  strk
- * Memory leak fixes. Improved performance by mean of more calls to 
- * new getCoordinatesRO() when applicable.
- *
- * Revision 1.22  2003/10/15 09:54:29  strk
- * Added getCoordinatesRO() public method.
- *
  **********************************************************************/
-
 
 #include <geos/geom.h>
 #include <algorithm>
@@ -304,7 +184,7 @@ bool LineString::equalsExact(const Geometry *other, double tolerance) const {
 	if (npts!=otherLineString->points->getSize()) {
 		return false;
 	}
-	for (int i=0; i<npts; i++) {
+	for (unsigned int i=0; i<npts; ++i) {
 		if (!equal(points->getAt(i),otherLineString->points->getAt(i),tolerance)) {
 			return false;
 		}
@@ -398,5 +278,129 @@ LineString::getGeometryTypeId() const {
 	return GEOS_LINESTRING;
 }
 
-}
+} // namespace geos
+
+/**********************************************************************
+ * $Log$
+ * Revision 1.52  2005/11/28 18:37:32  strk
+ * Minor warning removal
+ *
+ * Revision 1.51  2005/11/24 23:09:15  strk
+ * CoordinateSequence indexes switched from int to the more
+ * the correct unsigned int. Optimizations here and there
+ * to avoid calling getSize() in loops.
+ * Update of all callers is not complete yet.
+ *
+ * Revision 1.50  2005/11/15 10:02:27  strk
+ * optimized envelope computation reducing virtual calls
+ *
+ * Revision 1.49  2005/11/10 09:33:17  strk
+ * Removed virtual overloading LineString::compareTo(LineString *)
+ *
+ * Revision 1.48  2005/06/23 14:22:33  strk
+ * Inlined and added missing ::clone() for Geometry subclasses
+ *
+ * Revision 1.47  2005/02/22 17:10:47  strk
+ * Reduced CoordinateSequence::getSize() calls.
+ *
+ * Revision 1.46  2004/12/03 22:52:56  strk
+ * enforced const return of CoordinateSequence::toVector() method to derivate classes.
+ *
+ * Revision 1.45  2004/11/23 16:22:49  strk
+ * Added ElevationMatrix class and components to do post-processing draping of overlayed geometries.
+ *
+ * Revision 1.44  2004/09/13 09:07:28  strk
+ * Ported fix in LineString::isCoordinate
+ *
+ * Revision 1.43  2004/09/12 03:51:27  pramsey
+ * Casting changes to allow OS/X compilation.
+ *
+ * Revision 1.42  2004/07/22 08:45:50  strk
+ * Documentation updates, memory leaks fixed.
+ *
+ * Revision 1.41  2004/07/19 13:19:30  strk
+ * Documentation fixes
+ *
+ * Revision 1.40  2004/07/08 19:34:49  strk
+ * Mirrored JTS interface of CoordinateSequence, factory and
+ * default implementations.
+ * Added DefaultCoordinateSequenceFactory::instance() function.
+ *
+ * Revision 1.39  2004/07/06 17:58:22  strk
+ * Removed deprecated Geometry constructors based on PrecisionModel and
+ * SRID specification. Removed SimpleGeometryPrecisionReducer capability
+ * of changing Geometry's factory. Reverted Geometry::factory member
+ * to be a reference to external factory.
+ *
+ * Revision 1.38  2004/07/05 14:23:03  strk
+ * More documentation cleanups.
+ *
+ * Revision 1.37  2004/07/05 10:50:20  strk
+ * deep-dopy construction taken out of Geometry and implemented only
+ * in GeometryFactory.
+ * Deep-copy geometry construction takes care of cleaning up copies
+ * on exception.
+ * Implemented clone() method for CoordinateSequence
+ * Changed createMultiPoint(CoordinateSequence) signature to reflect
+ * copy semantic (by-ref instead of by-pointer).
+ * Cleaned up documentation.
+ *
+ * Revision 1.36  2004/07/03 12:51:37  strk
+ * Documentation cleanups for DoxyGen.
+ *
+ * Revision 1.35  2004/07/02 13:28:26  strk
+ * Fixed all #include lines to reflect headers layout change.
+ * Added client application build tips in README.
+ *
+ * Revision 1.34  2004/07/01 14:12:44  strk
+ *
+ * Geometry constructors come now in two flavors:
+ * 	- deep-copy args (pass-by-reference)
+ * 	- take-ownership of args (pass-by-pointer)
+ * Same functionality is available through GeometryFactory,
+ * including buildGeometry().
+ *
+ * Revision 1.33  2004/06/28 21:11:43  strk
+ * Moved getGeometryTypeId() definitions from geom.h to each geometry module.
+ * Added holes argument check in Polygon.cpp.
+ *
+ * Revision 1.32  2004/06/15 20:38:44  strk
+ * updated to respect deep-copy GeometryCollection interface
+ *
+ * Revision 1.31  2004/05/07 09:05:13  strk
+ * Some const correctness added. Fixed bug in GeometryFactory::createMultiPoint
+ * to handle NULL CoordinateSequence.
+ *
+ * Revision 1.30  2004/04/20 13:24:15  strk
+ * More leaks removed.
+ *
+ * Revision 1.29  2004/04/20 08:52:01  strk
+ * GeometryFactory and Geometry const correctness.
+ * Memory leaks removed from SimpleGeometryPrecisionReducer
+ * and GeometryFactory.
+ *
+ * Revision 1.28  2004/04/10 22:41:24  ybychkov
+ * "precision" upgraded to JTS 1.4
+ *
+ * Revision 1.27  2004/04/01 10:44:33  ybychkov
+ * All "geom" classes from JTS 1.3 upgraded to JTS 1.4
+ *
+ * Revision 1.26  2004/03/31 07:50:37  ybychkov
+ * "geom" partially upgraded to JTS 1.4
+ *
+ * Revision 1.25  2003/11/07 01:23:42  pramsey
+ * Add standard CVS headers licence notices and copyrights to all cpp and h
+ * files.
+ *
+ * Revision 1.24  2003/10/31 16:36:04  strk
+ * Re-introduced clone() method. Copy constructor could not really replace it.
+ *
+ * Revision 1.23  2003/10/16 08:50:00  strk
+ * Memory leak fixes. Improved performance by mean of more calls to 
+ * new getCoordinatesRO() when applicable.
+ *
+ * Revision 1.22  2003/10/15 09:54:29  strk
+ * Added getCoordinatesRO() public method.
+ *
+ **********************************************************************/
 
