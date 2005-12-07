d4b594a41549d11466fc6547f1cd321b8fb74159
Sandro Santilli <strk@keybit.net>
2005-12-07 23:36:49 +0000

    standard indentation
    git-svn-id: http://svn.osgeo.org/geos/trunk@966 5242fede-7e19-0410-aef8-94bd7d2200fb

--- source/geomgraph/EdgeIntersection.cpp
+++ source/geomgraph/EdgeIntersection.cpp
@@ -11,33 +11,7 @@
  * by the Free Software Foundation. 
  * See the COPYING file for more information.
  *
- **********************************************************************
- * $Log$
- * Revision 1.5  2005/11/16 22:21:45  strk
- * enforced const-correctness and use of initializer lists.
- *
- * Revision 1.4  2005/11/07 12:31:24  strk
- * Changed EdgeIntersectionList to use a set<> rathern then a vector<>, and
- * to avoid dynamic allocation of initial header.
- * Inlined short SweepLineEvent methods.
- *
- * Revision 1.3  2005/01/28 09:47:51  strk
- * Replaced sprintf uses with ostringstream.
- *
- * Revision 1.2  2004/07/02 13:28:26  strk
- * Fixed all #include lines to reflect headers layout change.
- * Added client application build tips in README.
- *
- * Revision 1.1  2004/03/19 09:48:45  ybychkov
- * "geomgraph" and "geomgraph/indexl" upgraded to JTS 1.4
- *
- * Revision 1.9  2003/11/07 01:23:42  pramsey
- * Add standard CVS headers licence notices and copyrights to all cpp and h
- * files.
- *
- *
  **********************************************************************/
-
 
 #include <sstream>
 #include <geos/geomgraph.h>
@@ -66,7 +40,9 @@ EdgeIntersection::compare(int newSegmentIndex, double newDist) const
 	return 0;
 }
 
-bool EdgeIntersection::isEndPoint(int maxSegmentIndex) {
+bool
+EdgeIntersection::isEndPoint(int maxSegmentIndex)
+{
 	if (segmentIndex==0 && dist==0.0) return true;
 	if (segmentIndex==maxSegmentIndex) return true;
 	return false;
@@ -89,4 +65,34 @@ EdgeIntersection::compareTo(const EdgeIntersection *other) const
 
 } // namespace geos
 
+
+/**********************************************************************
+ * $Log$
+ * Revision 1.6  2005/12/07 23:36:49  strk
+ * standard indentation
+ *
+ * Revision 1.5  2005/11/16 22:21:45  strk
+ * enforced const-correctness and use of initializer lists.
+ *
+ * Revision 1.4  2005/11/07 12:31:24  strk
+ * Changed EdgeIntersectionList to use a set<> rathern then a vector<>, and
+ * to avoid dynamic allocation of initial header.
+ * Inlined short SweepLineEvent methods.
+ *
+ * Revision 1.3  2005/01/28 09:47:51  strk
+ * Replaced sprintf uses with ostringstream.
+ *
+ * Revision 1.2  2004/07/02 13:28:26  strk
+ * Fixed all #include lines to reflect headers layout change.
+ * Added client application build tips in README.
+ *
+ * Revision 1.1  2004/03/19 09:48:45  ybychkov
+ * "geomgraph" and "geomgraph/indexl" upgraded to JTS 1.4
+ *
+ * Revision 1.9  2003/11/07 01:23:42  pramsey
+ * Add standard CVS headers licence notices and copyrights to all cpp and h
+ * files.
+ *
+ *
+ **********************************************************************/
 
