af30359f6e718960d641d26cc568f50ead3564b6
Sandro Santilli <strk@keybit.net>
2005-12-13 23:03:25 +0000

    Removed conditional blocks based on GEOS version (we're always in sync). This fixed an annoying problem introduced since INTERFACE_LAST and INTERFACE_CURREN were taken off geos/version.h
    git-svn-id: http://svn.osgeo.org/geos/trunk@988 5242fede-7e19-0410-aef8-94bd7d2200fb

--- source/capi/geos_c.cpp
+++ source/capi/geos_c.cpp
@@ -456,12 +456,9 @@ GEOSRelate(const Geometry *g1, const Geometry *g2)
 char
 GEOSisValid(const Geometry *g1)
 {
-#if GEOS_FIRST_INTERFACE <= 3 && GEOS_LAST_INTERFACE >= 3
 	IsValidOp ivo(g1);
-#endif
 	bool result;
 	try {
-#if GEOS_FIRST_INTERFACE <= 3 && GEOS_LAST_INTERFACE >= 3
 		result = ivo.isValid();
 		if ( result == 0 )
 		{
@@ -471,9 +468,7 @@ GEOSisValid(const Geometry *g1)
 				NOTICE_MESSAGE((char *)errmsg.c_str());
 			}
 		}
-#else // GEOS_INTERFACE 3 not supported
 		result = g1->isValid();
-#endif
 		return result;
 	}
 	catch (GEOSException *ge)
@@ -1558,16 +1553,8 @@ GEOSversion()
 char *
 GEOSjtsport()
 {
-#if GEOS_LAST_INTERFACE < 2
-	/*
-	 * GEOS upgrade needs postgis re-build, so this static
-	 * assignment is not going to be a problem
-	 */
-	char *res = strdup("1.3");
-#else
 	string version = jtsport();
 	char *res = strdup(version.c_str());
-#endif
 	return res;
 }
 
