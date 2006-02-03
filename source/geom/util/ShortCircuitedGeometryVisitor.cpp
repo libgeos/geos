b20a33aab6c055d686e90732def3aec3b8b3c5b8
Sandro Santilli <strk@keybit.net>
2006-02-03 09:26:46 +0000

    Added missing file
    git-svn-id: http://svn.osgeo.org/geos/trunk@1038 5242fede-7e19-0410-aef8-94bd7d2200fb

---
+++ source/geom/util/ShortCircuitedGeometryVisitor.cpp
@@ -0,0 +1,53 @@
+/**********************************************************************
+ * $Id$
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.refractions.net
+ *
+ * Copyright (C) 2006 Refractions Research Inc.
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation. 
+ * See the COPYING file for more information.
+ *
+ **********************************************************************
+ *
+ * Last port: geom/util/ShortCircuitedGeometryVisitor.java rev. 1.1 (JTS-1.7)
+ *
+ **********************************************************************/
+
+
+#include <geos/geom.h>
+#include <geos/geomUtil.h>
+
+namespace geos {
+namespace geom { // geos.geom
+namespace util { // geos.geom.util
+
+void
+ShortCircuitedGeometryVisitor::applyTo(const Geometry &geom)
+{
+	for (unsigned int i=0, n=geom.getNumGeometries(); i<n; ++i)
+	{
+		const Geometry *element = geom.getGeometryN(i);
+		if (dynamic_cast<const GeometryCollection*>(element))
+		{
+			applyTo(*element);
+		}
+		else
+		{
+			// calls the abstract virtual
+			visit(*element);
+			if (isDone()) done = true;
+		}
+
+		if ( done ) return;
+	}
+}
+
+
+} // namespace geos.geom.util
+} // namespace geos.geom
+} // namespace geos
+
