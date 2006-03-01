77212b9a0830dd9765bd1ea8f16a6f865059640c
Sandro Santilli <strk@keybit.net>
2006-03-01 08:52:21 +0000

    File to be included by Makefile.vc
    git-svn-id: http://svn.osgeo.org/geos/trunk@1096 5242fede-7e19-0410-aef8-94bd7d2200fb

---
+++ source/dirlist.mk
@@ -0,0 +1,36 @@
+# $Id$
+#
+# List of directories use by makefile.vc
+#
+
+GEOS_DIRLIST =  algorithm \
+                examples \
+                geom \
+                geom\util \
+                geomgraph \
+                geomgraph\index \
+                headers \
+                headers\geos \
+                index \
+                index\bintree \
+                index\chain \
+                index\quadtree \
+                index\strtree \
+                index\sweepline \
+                io \
+                noding \
+                noding\snapround \
+                operation \
+                operation\buffer \
+                operation\distance \
+                operation\linemerge \
+                operation\overlay \
+                operation\polygonize \
+                operation\predicate \
+                operation\relate \
+                operation\valid \
+                planargraph \
+                planargraph\algorithm \
+                precision \
+                util
+
