295d97803ad26ddf5c39cd2c9cace85c9de1f426
Sandro Santilli <strk@keybit.net>
2006-03-28 14:01:27 +0000

    forgot to add in previous commit
    git-svn-id: http://svn.osgeo.org/geos/trunk@1309 5242fede-7e19-0410-aef8-94bd7d2200fb

---
+++ source/io/ByteOrderDataInStream.cpp
@@ -0,0 +1,29 @@
+/**********************************************************************
+ * $Id$
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.refractions.net
+ *
+ * Copyright (C) 2005-2006 Refractions Research Inc.
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation. 
+ * See the COPYING file for more information.
+ *
+ **********************************************************************
+ *
+ * Last port: 
+ *
+ **********************************************************************/
+
+#ifndef GEOS_INLINE
+#include <geos/io/ByteOrderDataInStream.inl>
+#endif
+
+namespace geos {
+namespace io { // geos::io
+
+} // namespace geos::io
+} // namespace geos
+
