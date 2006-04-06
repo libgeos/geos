e49db0f96989d4f854580ced22d01c6650fae42e
Sandro Santilli <strk@keybit.net>
2006-04-06 17:33:16 +0000

    Added unit test for DoubleBits
    git-svn-id: http://svn.osgeo.org/geos/trunk@1375 5242fede-7e19-0410-aef8-94bd7d2200fb

---
+++ tests/unit/index/quadtree/DoubleBitsTest.cpp
@@ -0,0 +1,38 @@
+// $Id$
+// 
+// Test Suite for geos::index::quadtree::DoubleBits class.
+
+// TUT
+#include <tut.h>
+// GEOS
+#include <geos/index/quadtree/DoubleBits.h>
+
+using namespace geos::index::quadtree;
+
+namespace tut
+{
+	// dummy data, not used
+	struct test_doublebits_data {};
+
+	typedef test_group<test_doublebits_data> group;
+	typedef group::object object;
+
+	group test_doublebits_group("geos::index::quadtree::DoubleBits");
+
+	//
+	// Test Cases
+	//
+
+	// 1 - Test exponent()
+	template<>
+	template<>
+	void object::test<1>()
+	{
+		ensure_equals(DoubleBits::exponent(-1), 0);
+		ensure_equals(DoubleBits::exponent(8.0), 3);
+		ensure_equals(DoubleBits::exponent(128.0), 7);
+	}
+
+
+} // namespace tut
+
