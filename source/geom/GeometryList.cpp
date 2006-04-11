e1588ed6bbd26ba334ec213c02476d8930278c84
Sandro Santilli <strk@keybit.net>
2006-04-11 09:41:26 +0000

    Initial implementation of a GeometryList class, to be used to manage lists of Geometry pointers.
    git-svn-id: http://svn.osgeo.org/geos/trunk@1430 5242fede-7e19-0410-aef8-94bd7d2200fb

---
+++ source/geom/GeometryList.cpp
@@ -0,0 +1,83 @@
+/**********************************************************************
+ * $Id$
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.refractions.net
+ *
+ * Copyright (C) 2006 Refractions Research Inc.
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation. 
+ * See the COPYING file for more information.
+ *
+ **********************************************************************
+ *
+ * Last port: ORIGINAL WORK
+ *
+ **********************************************************************/
+
+#include <geos/geom/GeometryList.h>
+#include <geos/geom/Geometry.h> // for auto_ptr
+
+#include <memory> // for auto_ptr
+#include <vector> 
+
+namespace geos {
+namespace geom { // geos.geom
+
+
+/*private*/
+GeometryList::GeometryList()
+{
+}
+
+/*private*/
+GeometryList::~GeometryList()
+{
+	for (std::vector<Geometry*>::size_type i=0, n=geoms.size(); i<n; i++)
+	{
+		delete geoms[i];
+	}
+}
+
+/*public static*/
+GeometryList::AutoPtr
+GeometryList::create()
+{
+	return GeometryList::AutoPtr(new GeometryList());
+}
+
+/*public*/
+void
+GeometryList::add(Geometry::AutoPtr geom)
+{
+	geoms.push_back(geom.release());
+}
+
+/*public*/
+Geometry *
+GeometryList::operator[] (GeometryList::size_type i)
+{
+	return geoms[i];
+}
+
+/*public*/
+const Geometry *
+GeometryList::operator[] (GeometryList::size_type i) const
+{
+	return geoms[i];
+}
+
+
+} // namespace geos.geom
+} // namespace geos
+
+/**********************************************************************
+ * $Log$
+ * Revision 1.1  2006/04/11 09:41:26  strk
+ * Initial implementation of a GeometryList class, to be used to manage
+ * lists of Geometry pointers.
+ *
+ **********************************************************************/
+
