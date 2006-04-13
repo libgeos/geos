c31bb57401fd83421b4d3c7047099895c028ee07
Sandro Santilli <strk@keybit.net>
2006-04-13 14:51:51 +0000

    Forced NDR output (to not rely on architecture). Removed CRs endings.
    git-svn-id: http://svn.osgeo.org/geos/trunk@1450 5242fede-7e19-0410-aef8-94bd7d2200fb

--- tests/unit/io/WKBReaderTest.cpp
+++ tests/unit/io/WKBReaderTest.cpp
@@ -4,11 +4,13 @@
 // Uses geos::io::WKTReader to check correctness 
 // Uses geos::io::WKBWriter to check correctness 
 // Currently only tests 2D geoms of all (7) types.
+// Currently only tests NDR WKB 
 
 // TUT
 #include <tut.h>
 // GEOS
 #include <geos/io/WKBReader.h>
+#include <geos/io/WKBConstants.h>
 #include <geos/io/WKBWriter.h>
 #include <geos/io/WKTReader.h>
 #include <geos/geom/PrecisionModel.h>
@@ -24,25 +26,36 @@ namespace tut
 	//
 
 	// dummy data, not used
-	struct test_wkbreader_data {};
+	struct test_wkbreader_data
+	{
+		geos::geom::PrecisionModel pm;
+		geos::geom::GeometryFactory gf;
+		geos::io::WKBReader wkbreader;
+		geos::io::WKBWriter xdrwkbwriter;
+		geos::io::WKBWriter ndrwkbwriter;
+		geos::io::WKTReader wktreader;
+
+		typedef std::auto_ptr<geos::geom::Geometry> GeomPtr;
+
+		test_wkbreader_data()
+			:
+			pm(1.0),
+			gf(&pm),
+			wkbreader(gf),
+			// 2D only, XDR (big endian)
+			xdrwkbwriter(2, geos::io::WKBConstants::wkbXDR),
+			// 2D only, NDR (little endian)
+			ndrwkbwriter(2, geos::io::WKBConstants::wkbNDR),
+			wktreader(&gf)
+		{}
+
+	};
 
 	typedef test_group<test_wkbreader_data> group;
 	typedef group::object object;
 
 	group test_wkbreader_group("geos::io::WKBReader");
 
-
-	// These are static to avoid namespace pollution
-	// The struct test_*_data above is probably there
-	// for the same reason...
-	//
-	static geos::geom::PrecisionModel pm(1.0);
-	static geos::geom::GeometryFactory gf(&pm);
-	static geos::io::WKBReader wkbreader(gf);
-	static geos::io::WKBWriter wkbwriter(2); // 2D only
-        static geos::io::WKTReader wktreader(&gf);
-
-	typedef std::auto_ptr<geos::geom::Geometry> GeomPtr;
 
 	//
 	// Test Cases
@@ -53,6 +66,7 @@ namespace tut
 	template<>
 	void object::test<1>()
 	{         
+		// NDR
 		std::stringstream sin("010100000000000000000000000000000000000000");
 
 		GeomPtr gWKB(wkbreader.readHEX(sin));
@@ -61,7 +75,8 @@ namespace tut
 		ensure( gWKB->equalsExact(gWKT.get()) );
 
 		std::stringstream sout;
-		wkbwriter.writeHEX(*gWKB, sout);
+
+		ndrwkbwriter.writeHEX(*gWKB, sout);
 
 		ensure_equals(sout.str(), sin.str());
 
@@ -72,6 +87,7 @@ namespace tut
 	template<>
 	void object::test<2>()
 	{         
+		// NDR
 		std::stringstream sin("010200000002000000000000000000F03F000000000000004000000000000008400000000000001040");
 
 		GeomPtr gWKB (wkbreader.readHEX(sin));
@@ -80,7 +96,7 @@ namespace tut
 		ensure( gWKB->equalsExact(gWKT.get()) );
 
 		std::stringstream sout;
-		wkbwriter.writeHEX(*gWKB, sout);
+		ndrwkbwriter.writeHEX(*gWKB, sout);
 
 		ensure_equals(sout.str(), sin.str());
 
@@ -91,6 +107,7 @@ namespace tut
 	template<>
 	void object::test<3>()
 	{         
+		// NDR
 		std::stringstream sin("0103000000020000000500000000000000000000000000000000000000000000000000244000000000000000000000000000002440000000000000244000000000000000000000000000002440000000000000000000000000000000000400000000000000000000400000000000000040000000000000004000000000000018400000000000001840000000000000104000000000000000400000000000000040");
 
 		GeomPtr gWKB (wkbreader.readHEX(sin));
@@ -99,7 +116,7 @@ namespace tut
 		ensure( gWKB->equalsExact(gWKT.get()) );
 
 		std::stringstream sout;
-		wkbwriter.writeHEX(*gWKB, sout);
+		ndrwkbwriter.writeHEX(*gWKB, sout);
 
 		ensure_equals(sout.str(), sin.str());
 	}
@@ -109,6 +126,7 @@ namespace tut
 	template<>
 	void object::test<4>()
 	{         
+		// NDR
 		std::stringstream sin("010400000005000000010100000000000000000000000000000000000000010100000000000000000024400000000000000000010100000000000000000024400000000000002440010100000000000000000000000000000000002440010100000000000000000000000000000000000000");
 
 		GeomPtr gWKB (wkbreader.readHEX(sin));
@@ -117,7 +135,7 @@ namespace tut
 		ensure( gWKB->equalsExact(gWKT.get()) );
 
 		std::stringstream sout;
-		wkbwriter.writeHEX(*gWKB, sout);
+		ndrwkbwriter.writeHEX(*gWKB, sout);
 
 		ensure_equals(sout.str(), sin.str());
 	}
@@ -127,6 +145,7 @@ namespace tut
 	template<>
 	void object::test<5>()
 	{         
+		// NDR
 		std::stringstream sin("010500000002000000010200000005000000000000000000000000000000000000000000000000002440000000000000000000000000000024400000000000002440000000000000000000000000000024400000000000002440000000000000344001020000000400000000000000000000400000000000000040000000000000004000000000000018400000000000001840000000000000104000000000000034400000000000000040");
 
 		GeomPtr gWKB (wkbreader.readHEX(sin));
@@ -135,7 +154,7 @@ namespace tut
 		ensure( gWKB->equalsExact(gWKT.get()) );
 
 		std::stringstream sout;
-		wkbwriter.writeHEX(*gWKB, sout);
+		ndrwkbwriter.writeHEX(*gWKB, sout);
 
 		ensure_equals(sout.str(), sin.str());
 	}
@@ -145,6 +164,7 @@ namespace tut
 	template<>
 	void object::test<6>()
 	{         
+		// NDR
 		std::stringstream sin("0106000000020000000103000000020000000500000000000000000000000000000000000000000000000000244000000000000000000000000000002440000000000000244000000000000000000000000000002440000000000000000000000000000000000400000000000000000000400000000000000040000000000000004000000000000018400000000000001840000000000000104000000000000000400000000000000040010300000001000000040000000000000000004E400000000000004E400000000000004E400000000000004940000000000080514000000000000044400000000000004E400000000000004E40");
 
 		GeomPtr gWKB (wkbreader.readHEX(sin));
@@ -153,7 +173,7 @@ namespace tut
 		ensure( gWKB->equalsExact(gWKT.get()) );
 
 		std::stringstream sout;
-		wkbwriter.writeHEX(*gWKB, sout);
+		ndrwkbwriter.writeHEX(*gWKB, sout);
 
 		ensure_equals(sout.str(), sin.str());
 	}
@@ -163,6 +183,7 @@ namespace tut
 	template<>
 	void object::test<7>()
 	{         
+		// NDR
 		std::stringstream sin("010700000006000000010100000000000000000000000000000000000000010200000002000000000000000000F03F00000000000000400000000000000840000000000000104001030000000200000005000000000000000000000000000000000000000000000000002440000000000000000000000000000024400000000000002440000000000000000000000000000024400000000000000000000000000000000004000000000000000000004000000000000000400000000000000040000000000000184000000000000018400000000000001040000000000000004000000000000000400104000000050000000101000000000000000000000000000000000000000101000000000000000000244000000000000000000101000000000000000000244000000000000024400101000000000000000000000000000000000024400101000000000000000000000000000000000000000105000000020000000102000000050000000000000000000000000000000000000000000000000024400000000000000000000000000000244000000000000024400000000000000000000000000000244000000000000024400000000000003440010200000004000000000000000000004000000000000000400000000000000040000000000000184000000000000018400000000000001040000000000000344000000000000000400106000000020000000103000000020000000500000000000000000000000000000000000000000000000000244000000000000000000000000000002440000000000000244000000000000000000000000000002440000000000000000000000000000000000400000000000000000000400000000000000040000000000000004000000000000018400000000000001840000000000000104000000000000000400000000000000040010300000001000000040000000000000000004E400000000000004E400000000000004E400000000000004940000000000080514000000000000044400000000000004E400000000000004E40");
 
 		GeomPtr gWKB(wkbreader.readHEX(sin));
@@ -171,7 +192,7 @@ namespace tut
 		ensure( gWKB->equalsExact(gWKT.get()) );
 
 		std::stringstream sout;
-		wkbwriter.writeHEX(*gWKB, sout);
+		ndrwkbwriter.writeHEX(*gWKB, sout);
 
 		ensure_equals(sout.str(), sin.str());
 	}
