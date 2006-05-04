879163183fa8d64f6ae790ba941e7956c29e5c25
Sandro Santilli <strk@keybit.net>
2006-05-04 12:54:26 +0000

    Added invariant tester for NodeMap class, fixed comment about ownership of NodeFactory
    git-svn-id: http://svn.osgeo.org/geos/trunk@1553 5242fede-7e19-0410-aef8-94bd7d2200fb

--- source/headers/geos/geomgraph/NodeMap.h
+++ source/headers/geos/geomgraph/NodeMap.h
@@ -23,6 +23,7 @@
 #include <string>
 
 #include <geos/geom/Coordinate.h> // for CoordinateLessThen
+#include <geos/geomgraph/Node.h> // for testInvariant
 
 #include <geos/inline.h>
 
@@ -42,29 +43,60 @@ class NodeMap{
 public:
 
 	typedef std::map<geom::Coordinate*,Node*,geom::CoordinateLessThen> container;
+
 	typedef container::iterator iterator;
+
 	typedef container::const_iterator const_iterator;
+
 	typedef std::pair<geom::Coordinate*,Node*> pair;
 
 	container nodeMap;
+
 	const NodeFactory &nodeFact;
-	// newNodeFact will be deleted by ~NodeMap
+
+	/// \brief
+	/// NodeMap will keep a reference to the NodeFactory,
+	/// keep it alive for the whole NodeMap lifetime
 	NodeMap(const NodeFactory &newNodeFact);
+
 	virtual ~NodeMap();
+
 	Node* addNode(const geom::Coordinate& coord);
+
 	Node* addNode(Node *n);
+
 	void add(EdgeEnd *e);
+
 	Node *find(const geom::Coordinate& coord) const;
 
 	const_iterator begin() const { return nodeMap.begin(); }
+
 	const_iterator end() const { return nodeMap.end(); }
+
 	iterator begin() { return nodeMap.begin(); }
+
 	iterator end() { return nodeMap.end(); }
 
 	void getBoundaryNodes(int geomIndex,
 		std::vector<Node*>&bdyNodes) const;
 
 	std::string print() const;
+
+	void testInvariant()
+	{
+#ifndef NDEBUG
+		// Each Coordinate key is a pointer inside the Node value
+		for (iterator it=begin(), itEnd=end(); it != itEnd; ++it)
+		{
+			pair p = *it;
+			geomgraph::Node* n = p.second;
+			geom::Coordinate* c = const_cast<geom::Coordinate*>(
+				&(n->getCoordinate())
+			);
+			assert(p.first == c);
+		}
+#endif
+	}
 };
 
 } // namespace geos.geomgraph
@@ -78,6 +110,9 @@ public:
 
 /**********************************************************************
  * $Log$
+ * Revision 1.3  2006/05/04 12:54:26  strk
+ * Added invariant tester for NodeMap class, fixed comment about ownership of NodeFactory
+ *
  * Revision 1.2  2006/03/24 09:52:41  strk
  * USE_INLINE => GEOS_INLINE
  *
