e1594ba1867b53e262fb0193c8fba73892bd44f5
Charlie Savage <cfis@savagexi.com>
2006-07-13 07:35:26 +0000

    New tests for the buffer operation.
    git-svn-id: http://svn.osgeo.org/geos/trunk@1757 5242fede-7e19-0410-aef8-94bd7d2200fb

---
+++ swig/ruby/test/test_buffer.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require 'test/unit'
+require 'geos'
+require 'test_helper'
+
+class TestBuffer < Test::Unit::TestCase
+  def setup
+    @test_helper = GeosTestHelper.new()
+  end
+  
+  def test_xml_cases
+    cases = @test_helper.load_tests("buffer.xml")
+    
+    cases.each do |test_case|
+      geom = test_case.geom1
+      tolerance = test_case.arg3.to_f
+      expected = @test_helper.parse_geometry(test_case.expected)
+      expected.normalize
+      
+      result = geom.buffer(tolerance, 8)
+      result.normalize
+      
+      assert(expected.equals_exact(result, 0.00000000001),
+             "Buffer:\n" + 
+             "  Case: #{test_case.case_id}\n" + 
+             "  Test: #{test_case.test_id}\n" + 
+             "  Geom: #{Geos.geom_to_wkt(geom)}\n" + 
+             "  Tolerance: #{tolerance}\n" + 
+             "  Expected: #{Geos.geom_to_wkt(expected)}.\n" + 
+             "    Actual: #{Geos.geom_to_wkt(result)}")
+    end
+  end    
+end
+
+
+
