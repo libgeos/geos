7174fe612c4047e0371c37b38cada2d32f79de47
Sandro Santilli <strk@keybit.net>
2006-11-03 10:30:06 +0000

    Report non-zero return from ./geostest run
    git-svn-id: http://svn.osgeo.org/geos/trunk@1894 5242fede-7e19-0410-aef8-94bd7d2200fb

--- capi/testrunner.sh
+++ capi/testrunner.sh
@@ -9,12 +9,19 @@ cat <<EOF > ${runner}
 #!/bin/sh
 ./geostest ${srcdir}/test.wkt > test.out
 ret=$?
+
+if test "$ret" -ne "0"; then
+	echo "geostest exited with non-zero status ($ret)"
+fi
+
 # It's enough that it doesn't segfault for now...
 # computing a diff doesn't currently work as geostest
 # was not initially made taking arch differences into
 # account
 diff ${srcdir}/test.expected test.out
+
 exit $ret
+
 EOF
 
 chmod +x ${runner}
