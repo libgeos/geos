1f1afeea642e36df630ce4f2ed0ca3230beaab78
Ben Jubb <benjubb@refractions.net>
2007-12-21 20:47:42 +0000

    Added from JTS 1.9 to support prepared geometry
    git-svn-id: http://svn.osgeo.org/geos/trunk@2070 5242fede-7e19-0410-aef8-94bd7d2200fb

---
+++ source/headers/geos/index/intervalrtree/IntervalRTreeLeafNode.h
@@ -0,0 +1,65 @@
+/**********************************************************************
+ * $Id
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.refractions.net
+ *
+ * Copyright (C) 2006 Refractions Research Inc.
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation. 
+ * See the COPYING file for more information.
+ *
+ *
+ **********************************************************************/
+
+#ifndef GEOS_INDEX_INTERVALRTREE_INTERVALRTREELEAFNODE_H
+#define GEOS_INDEX_INTERVALRTREE_INTERVALRTREELEAFNODE_H
+
+
+#include <geos/index/intervalrtree/IntervalRTreeNode.h> // inherited
+
+
+// forward declarations
+namespace geos {
+	namespace index {
+		class ItemVisitor;
+	}
+}
+
+
+namespace geos {
+namespace index {
+namespace intervalrtree {
+
+class IntervalRTreeLeafNode : public IntervalRTreeNode
+{
+private:
+	void * item;
+
+protected:
+public:
+	IntervalRTreeLeafNode( double min, double max, void * item)
+	:	IntervalRTreeNode( min, max),
+		item( item)
+	{ }
+
+	~IntervalRTreeLeafNode()
+	{
+		delete item;
+	}
+	
+	void query( double queryMin, double queryMax, index::ItemVisitor * visitor) const;
+
+};
+
+} // geos::intervalrtree
+} // geos::index
+} // geos
+
+#endif // GEOS_INDEX_INTERVALRTREE_INTERVALRTREELEAFNODE_H
+/**********************************************************************
+ * $Log$
+ **********************************************************************/
+
