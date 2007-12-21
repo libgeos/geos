133124ff22328260b35eabf2d0b6acf52ae9ba2a
Ben Jubb <benjubb@refractions.net>
2007-12-21 20:44:58 +0000

    Added from JTS 1.9 to support prepared geometry
    git-svn-id: http://svn.osgeo.org/geos/trunk@2068 5242fede-7e19-0410-aef8-94bd7d2200fb

---
+++ source/geom/util/ComponentCoordinateExtracter.cpp
@@ -0,0 +1,31 @@
+/**********************************************************************
+ * $Id:
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.refractions.net
+ *
+ * Copyright (C) 2001-2002 Vivid Solutions Inc.
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation. 
+ * See the COPYING file for more information.
+ *
+ **********************************************************************/
+
+
+#include <geos/geom/util/ComponentCoordinateExtracter.h>
+#include <typeinfo>
+
+namespace geos {
+namespace geom { // geos.geom
+namespace util { // geos.geom.util
+
+} // namespace geos.geom.util
+} // namespace geos.geom
+} // namespace geos
+
+/**********************************************************************
+ * $Log$
+ *
+ **********************************************************************/
