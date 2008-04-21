e6a54034c7e4d60c4883a42a3eec0dee708b4fa7
Paul Ramsey <pramsey@cleverelephant.ca>
2008-04-21 23:22:11 +0000

    Fix for rectangle optimization logic.
    git-svn-id: http://svn.osgeo.org/geos/branches/3.0@2124 5242fede-7e19-0410-aef8-94bd7d2200fb

--- source/geom/Geometry.cpp
+++ source/geom/Geometry.cpp
@@ -422,9 +422,11 @@ Geometry::contains(const Geometry *g) const
 	if (isRectangle()) {
 		return predicate::RectangleContains::contains((Polygon&)*this, *g);
 	}
+/* Removed per: http://www.nabble.com/ST_Contains-doesn't-work--to14946025.html
 	if (g->isRectangle()) {
 		return predicate::RectangleContains::contains((const Polygon&)*g, *this);
 	}
+*/
 
 	IntersectionMatrix *im=relate(g);
 	bool res=im->isContains();
