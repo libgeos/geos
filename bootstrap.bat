a383316bff23289055ae948c4ae56d24e8e43d74
Mateusz Loskot <mateusz@loskot.net>
2008-05-20 20:33:50 +0000

    * bootstrap.bat: Added script generating headers for use with Visual C++ compiler. DO NOT include this script in GEOS source distribution. * source/makefile.vc: Do not make copies of 3 dynamic headers but let users to use bootstrap.bat instead.
    git-svn-id: http://svn.osgeo.org/geos/trunk@2125 5242fede-7e19-0410-aef8-94bd7d2200fb

---
+++ bootstrap.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+REM $Id$
+REM
+REM This script generates headers for use with Visual C++ only
+REM
+REM Usage:
+REM .\bootstrap.bat
+REM nmake -f makefile.vc
+REM
+REM NOTE: DO NOT include this script in GEOS sources distribution
+REM
+
+set GEOS_HEADERS=source\headers\geos
+
+COPY %GEOS_HEADERS%\version.h.vc %GEOS_HEADERS%\version.h 
+COPY %GEOS_HEADERS%\platform.h.vc %GEOS_HEADERS%\platform.h
+COPY capi\geos_c.h.in capi\geos_c.h
