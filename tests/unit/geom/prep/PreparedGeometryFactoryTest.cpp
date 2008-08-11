89b8122168cf618a171bd0d74225a224b494449c
Mateusz Loskot <mateusz@loskot.net>
2008-08-11 23:54:57 +0000

    Refactored tabs vs spaces in PreparedGeometryFactoryTest.
    git-svn-id: http://svn.osgeo.org/geos/trunk@2153 5242fede-7e19-0410-aef8-94bd7d2200fb

--- tests/unit/geom/prep/PreparedGeometryFactoryTest.cpp
+++ tests/unit/geom/prep/PreparedGeometryFactoryTest.cpp
@@ -19,87 +19,86 @@ using namespace geos::geom;
 
 namespace tut
 {
-	//
-	// Test Group
-	//
+    //
+    // Test Group
+    //
 
-	// Common data used by tests
-	struct test_preparedgeometryfactory_data
-	{
+    // Common data used by tests
+    struct test_preparedgeometryfactory_data
+    {
     };
 
-	typedef test_group<test_preparedgeometryfactory_data> group;
-	typedef group::object object;
+    typedef test_group<test_preparedgeometryfactory_data> group;
+    typedef group::object object;
 
-	group test_preparedgeometryfactory_data("geos::geom::prep::PreparedGeometryFactory");
+    group test_preparedgeometryfactory_data("geos::geom::prep::PreparedGeometryFactory");
 
-	//
-	// Test Cases
-	//
+    //
+    // Test Cases
+    //
 
-	// Test of default constructor
-	template<>
-	template<>
-	void object::test<1>()
-	{
-		prep::PreparedGeometryFactory pgf;
-	}
-	
+    // Test of default constructor
+    template<>
+    template<>
+    void object::test<1>()
+    {
+        prep::PreparedGeometryFactory pgf;
+    }
+
     // Test passing null-pointer to prepare static method
-	template<>
-	template<>
-	void object::test<2>()
-	{
+    template<>
+    template<>
+    void object::test<2>()
+    {
         // FIXME: null pointer throws segfault (Ticket #197)
         // ensure(0 == prep::PreparedGeometryFactory::prepare(0));
-	}
-	
+    }
+
     // Test passing null-pointer to create method
-	template<>
-	template<>
-	void object::test<3>()
-	{
-		prep::PreparedGeometryFactory pgf;
+    template<>
+    template<>
+    void object::test<3>()
+    {
+        prep::PreparedGeometryFactory pgf;
 
         // FIXME: null pointer throws segfault (Ticket #197)
         // ensure(0 == pgf.create(0));
-	}
-	
+    }
+
     // Test prepare with empty POINT
-	template<>
-	template<>
-	void object::test<4>()
-	{
-		GeometryFactory gf;
-		Geometry* g = gf.createEmptyGeometry();
-		ensure( 0 != g );
-		
+    template<>
+    template<>
+    void object::test<4>()
+    {
+        GeometryFactory gf;
+        Geometry* g = gf.createEmptyGeometry();
+        ensure( 0 != g );
+        
         prep::PreparedGeometry const* pg = prep::PreparedGeometryFactory::prepare(g);
-		ensure( 0 != pg );
+        ensure( 0 != pg );
 
         // FREE MEMORY
         prep::PreparedGeometryFactory::destroy(pg);
         gf.destroyGeometry(g);
-	}
-	
+    }
+
     // Test create with empty POINT
-	template<>
-	template<>
-	void object::test<5>()
-	{
-		GeometryFactory gf;
-		Geometry* g = gf.createEmptyGeometry();
-		ensure( 0 != g );
-		
-		prep::PreparedGeometryFactory pgf;
-		prep::PreparedGeometry const* pg = pgf.create(g);
-		ensure( 0 != pg );
+    template<>
+    template<>
+    void object::test<5>()
+    {
+        GeometryFactory gf;
+        Geometry* g = gf.createEmptyGeometry();
+        ensure( 0 != g );
+        
+        prep::PreparedGeometryFactory pgf;
+        prep::PreparedGeometry const* pg = pgf.create(g);
+        ensure( 0 != pg );
         
         // FREE MEMORY
         prep::PreparedGeometryFactory::destroy(pg);
         gf.destroyGeometry(g);
-	}
-	
+    }
 
 } // namespace tut
 
