daba2f376ed1cdea761be7816d9b443b0c89a26e
Mateusz Loskot <mateusz@loskot.net>
2008-08-18 23:45:34 +0000

    tests/unit/algorithm/CGAlgorithms: Fixed number of memory leaks.
    git-svn-id: http://svn.osgeo.org/geos/trunk@2165 5242fede-7e19-0410-aef8-94bd7d2200fb

--- tests/unit/algorithm/CGAlgorithms/isCCWTest.cpp
+++ tests/unit/algorithm/CGAlgorithms/isCCWTest.cpp
@@ -8,12 +8,14 @@
 // geos
 #include <geos/algorithm/CGAlgorithms.h>
 #include <geos/geom/Polygon.h>
-#include <geos/geom/Coordinate.h>
 #include <geos/geom/Geometry.h>
+#include <geos/geom/CoordinateSequence.h>
+#include <geos/geom/Coordinate.h>
 #include <geos/io/WKTReader.h>
 // std
 #include <string>
 #include <memory>
+#include <cassert>
 
 using namespace geos::algorithm;
 
@@ -27,9 +29,20 @@ namespace tut
     {
 	    typedef std::auto_ptr<geos::geom::Geometry> GeometryPtr;
 
+        geos::geom::CoordinateSequence* cs_;
         geos::io::WKTReader reader_;
 
-        test_isccw_data() {}
+        test_isccw_data()
+            : cs_(0)
+        {
+            assert(0 == cs_);
+        }
+        
+        ~test_isccw_data()
+        {
+            delete cs_;
+            cs_ = 0;
+        }
     };
 
     typedef test_group<test_isccw_data> group;
@@ -49,7 +62,8 @@ namespace tut
         const std::string wkt("POLYGON ((60 180, 140 240, 140 240, 140 240, 200 180, 120 120, 60 180))");
 		GeometryPtr geom(reader_.read(wkt));
 
-        bool isCCW = CGAlgorithms::isCCW(geom->getCoordinates());
+        cs_ = geom->getCoordinates();
+        bool isCCW = CGAlgorithms::isCCW(cs_);
 
         ensure_equals( false, isCCW );
     }
@@ -62,7 +76,8 @@ namespace tut
         const std::string wkt("POLYGON ((60 180, 140 120, 100 180, 140 240, 60 180))");
 		GeometryPtr geom(reader_.read(wkt));
 
-        bool isCCW = CGAlgorithms::isCCW(geom->getCoordinates());
+        cs_ = geom->getCoordinates();
+        bool isCCW = CGAlgorithms::isCCW(cs_);
 
         ensure_equals( true, isCCW );
     }
@@ -75,9 +90,11 @@ namespace tut
         const std::string wkt("POLYGON ((60 180, 140 120, 100 180, 140 240, 140 240, 60 180))");
 		GeometryPtr geom(reader_.read(wkt));
 
-        bool isCCW = CGAlgorithms::isCCW(geom->getCoordinates());
+        cs_ = geom->getCoordinates();
+        bool isCCW = CGAlgorithms::isCCW(cs_);
 
         ensure_equals( true, isCCW );
     }
 
 } // namespace tut
+
