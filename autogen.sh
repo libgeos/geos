4bb8951c52f2433defb6c1e83fac248dd1495615
Mateusz Loskot <mateusz@loskot.net>
2009-03-23 14:01:54 +0000

    autogen.sh: verbose check if autotools versions.
    git-svn-id: http://svn.osgeo.org/geos/trunk@2289 5242fede-7e19-0410-aef8-94bd7d2200fb

--- autogen.sh
+++ autogen.sh
@@ -14,16 +14,24 @@ giveup()
 
 OSTYPE=`uname -s`
 
+AUTOHEADER=`which autoheader 2>/dev/null`
+if [ ! ${AUTOHEADER} ]; then
+    echo "Missing autoconf!"
+    exit
+fi
+AUTOHEADER_VER=`${AUTOHEADER} --version | grep -E "^.*[0-9]$" | sed 's/^.* //'`
+
 for aclocal in aclocal aclocal-1.10 aclocal-1.9; do
     ACLOCAL=`which $aclocal 2>/dev/null`
     if test -x "${ACLOCAL}"; then
         break;
     fi
 done
-if [ ! $ACLOCAL ]; then
+if [ ! ${ACLOCAL} ]; then
     echo "Missing aclocal!"
     exit
 fi
+ACLOCAL_VER=`${ACLOCAL} --version | grep -E "^.*[0-9]$" | sed 's/^.* //'`
 
 for automake in automake automake-1.10 automake-1.9; do
     AUTOMAKE=`which $automake 2>/dev/null`
@@ -31,10 +39,11 @@ for automake in automake automake-1.10 automake-1.9; do
         break;
     fi
 done
-if [ ! $AUTOMAKE ]; then
+if [ ! ${AUTOMAKE} ]; then
     echo "Missing automake!"
     exit
 fi
+AUTOMAKE_VER=`${AUTOMAKE} --version | grep -E "^.*[0-9]$" | sed 's/^.* //'`
 
 
 for libtoolize in glibtoolize libtoolize; do
@@ -43,10 +52,11 @@ for libtoolize in glibtoolize libtoolize; do
         break;
     fi
 done
-if [ ! $LIBTOOLIZE ]; then
+if [ ! ${LIBTOOLIZE} ]; then
     echo "Missing libtoolize!"
     exit
 fi
+LIBTOOLIZE_VER=`${LIBTOOLIZE} --version | grep -E "^.*[0-9]\.[0-9]$" | sed 's/^.* //'`
 
 #AMFLAGS="--add-missing --copy --force-missing"
 AMFLAGS="--add-missing --copy"
@@ -54,15 +64,16 @@ if test "$OSTYPE" = "IRIX" -o "$OSTYPE" = "IRIX64"; then
    AMFLAGS=$AMFLAGS" --include-deps";
 fi
 
-echo "Running $ACLOCAL -I macros"
-$ACLOCAL -I macros || giveup
-echo "Running autoheader"
-autoheader || giveup
-echo "Running $LIBTOOLIZE --force --copy"
-$LIBTOOLIZE --force --copy || giveup
-echo "Running $AUTOMAKE $AMFLAGS"
-$AUTOMAKE $AMFLAGS # || giveup
-echo "Running autoconf"
+echo "* Running $ACLOCAL (${ACLOCAL_VER})"
+${ACLOCAL} -I macros || giveup
+echo "* Running ${AUTOHEADER} (${AUTOHEADER_VER})"
+${AUTOHEADER} || giveup
+echo "* Running ${LIBTOOLIZE} (${LIBTOOLIZE_VER})"
+${LIBTOOLIZE} --force --copy || giveup
+echo "* Running ${AUTOMAKE} (${AUTOMAKE_VER})"
+echo "\tAMFLAGS=${AMFLAGS}"
+${AUTOMAKE} ${AMFLAGS} # || giveup
+echo "* Running autoconf"
 autoconf || giveup
 
 echo "======================================"
