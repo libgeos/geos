423f4317644cefbbd42c22e712caf26c92111f64
Sandro Santilli <strk@keybit.net>
2009-04-08 10:06:56 +0000

    Add a couple more tests to SegmentNode unit test
    git-svn-id: http://svn.osgeo.org/geos/trunk@2321 5242fede-7e19-0410-aef8-94bd7d2200fb

--- tests/unit/noding/SegmentNodeTest.cpp
+++ tests/unit/noding/SegmentNodeTest.cpp
@@ -75,11 +75,12 @@ namespace tut
         // Construct a node on the given NodedSegmentString
         {
             const size_t segment_index = 0;
-            Coordinate cint(3, 3);
-            SegmentNode node( segment, cint, segment_index,
+            SegmentNode node( segment, Coordinate(3,3), segment_index,
                               segment.getSegmentOctant(segment_index) );
 
             ensure_equals( node.segmentIndex, segment_index );
+
+            // only first endpoint is considered interior
             ensure( node.isInterior() ); 
 
             // 
@@ -89,6 +90,133 @@ namespace tut
             //
 
         }
+
+    }
+
+    template<>
+    template<>
+    void object::test<2>()
+    {
+        using geos::geom::Coordinate;
+        using geos::noding::NodedSegmentString;
+        using geos::noding::SegmentNode;
+
+        // Create coordinates sequence
+        const size_t coords_size = 2;
+        CoordSeqPtr cs( factory_->create(0, coords_size) );
+
+        ensure( 0 != cs.get() );
+
+        Coordinate c0(0, 0);
+        Coordinate c1(3, 3);
+        cs->add(c0);
+        cs->add(c1);
+
+        ensure_equals( cs->size(), coords_size );
+
+        // Create SegmentString instance
+        
+        NodedSegmentString segment(cs.get(), 0);
+
+        ensure_equals( segment.size(), coords_size );
+
+        // Construct an interior node on the given NodedSegmentString
+        {
+            const size_t segment_index = 0;
+            SegmentNode node( segment, Coordinate(0,0), segment_index,
+                              segment.getSegmentOctant(segment_index) );
+
+            ensure_equals( node.segmentIndex, segment_index );
+
+            // on first endpoint ...
+            ensure( ! node.isInterior() ); 
+
+        }
+
+    }
+
+    template<>
+    template<>
+    void object::test<3>()
+    {
+        using geos::geom::Coordinate;
+        using geos::noding::NodedSegmentString;
+        using geos::noding::SegmentNode;
+
+        // Create coordinates sequence
+        const size_t coords_size = 2;
+        CoordSeqPtr cs( factory_->create(0, coords_size) );
+
+        ensure( 0 != cs.get() );
+
+        Coordinate c0(0, 0);
+        Coordinate c1(3, 3);
+        cs->add(c0);
+        cs->add(c1);
+
+        ensure_equals( cs->size(), coords_size );
+
+        // Create SegmentString instance
+        
+        NodedSegmentString segment(cs.get(), 0);
+
+        ensure_equals( segment.size(), coords_size );
+
+        // Construct an interior node on the given NodedSegmentString
+        {
+            const size_t segment_index = 0;
+            SegmentNode node( segment, Coordinate(2,2), segment_index,
+                              segment.getSegmentOctant(segment_index) );
+
+            ensure_equals( node.segmentIndex, segment_index );
+
+            // on first endpoint ...
+            ensure( node.isInterior() ); 
+
+        }
+
+    }
+
+    template<>
+    template<>
+    void object::test<4>()
+    {
+        using geos::geom::Coordinate;
+        using geos::noding::NodedSegmentString;
+        using geos::noding::SegmentNode;
+
+        // Create coordinates sequence
+        const size_t coords_size = 2;
+        CoordSeqPtr cs( factory_->create(0, coords_size) );
+
+        ensure( 0 != cs.get() );
+
+        Coordinate c0(0, 0);
+        Coordinate c1(3, 3);
+        cs->add(c0);
+        cs->add(c1);
+
+        ensure_equals( cs->size(), coords_size );
+
+        // Create SegmentString instance
+        
+        NodedSegmentString segment(cs.get(), 0);
+
+        ensure_equals( segment.size(), coords_size );
+
+        // Construct a node that doesn't even intersect !!
+        {
+            const size_t segment_index = 0;
+            SegmentNode node( segment, Coordinate(1,2), segment_index,
+                              segment.getSegmentOctant(segment_index) );
+
+            ensure_equals( node.segmentIndex, segment_index );
+
+            // on first endpoint ...
+            ensure( node.isInterior() ); 
+
+        }
+
     }
 
 } // namespace tut
