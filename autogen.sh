009bc3e78c599f1efa148a72f24acfb8ab6fe6eb
Paul Ramsey <pramsey@cleverelephant.ca>
2009-04-08 16:33:32 +0000

    Patched up autogen script
    git-svn-id: http://svn.osgeo.org/geos/branches/3.1@2334 5242fede-7e19-0410-aef8-94bd7d2200fb

--- autogen.sh
+++ autogen.sh
@@ -14,16 +14,30 @@ giveup()
 
 OSTYPE=`uname -s`
 
+AUTOCONF=`which autoconf 2>/dev/null`
+if [ ! ${AUTOCONF} ]; then
+    echo "Missing autoconf!"
+    exit
+fi
+AUTOCONF_VER=`${AUTOCONF} --version | grep -E "^.*[0-9]$" | sed 's/^.* //'`
+
+AUTOHEADER=`which autoheader 2>/dev/null`
+if [ ! ${AUTOHEADER} ]; then
+    echo "Missing autoheader!"
+    exit
+fi
+
 for aclocal in aclocal aclocal-1.10 aclocal-1.9; do
     ACLOCAL=`which $aclocal 2>/dev/null`
     if test -x "${ACLOCAL}"; then
         break;
     fi
 done
-if [ ! $ACLOCAL ]; then
-    echo "Missingn aclocal!"
+if [ ! ${ACLOCAL} ]; then
+    echo "Missing aclocal!"
     exit
 fi
+ACLOCAL_VER=`${ACLOCAL} --version | grep -E "^.*[0-9]$" | sed 's/^.* //'`
 
 for automake in automake automake-1.10 automake-1.9; do
     AUTOMAKE=`which $automake 2>/dev/null`
@@ -31,40 +45,54 @@ for automake in automake automake-1.10 automake-1.9; do
         break;
     fi
 done
-if [ ! $AUTOMAKE ]; then
-    echo "Missingn automake!"
+if [ ! ${AUTOMAKE} ]; then
+    echo "Missing automake!"
     exit
 fi
+AUTOMAKE_VER=`${AUTOMAKE} --version | grep -E "^.*[0-9]$" | sed 's/^.* //'`
 
 
-for libtoolize in glibtoolize libtoolize; do
+for libtoolize in libtoolize glibtoolize; do
     LIBTOOLIZE=`which $libtoolize 2>/dev/null`
     if test -x "${LIBTOOLIZE}"; then
         break;
     fi
 done
-if [ ! $LIBTOOLIZE ]; then
-    echo "Missingn libtoolize!"
+if [ ! ${LIBTOOLIZE} ]; then
+    echo "Missing libtoolize!"
     exit
 fi
+LIBTOOLIZE_VER=`${LIBTOOLIZE} --version | grep -E "^.*[0-9]\.[0-9]" | sed 's/^.* //'`
 
-#AMFLAGS="--add-missing --copy --force-missing"
-AMFLAGS="--add-missing --copy"
+AMOPTS="--add-missing --copy"
 if test "$OSTYPE" = "IRIX" -o "$OSTYPE" = "IRIX64"; then
-   AMFLAGS=$AMFLAGS" --include-deps";
+   AMOPTS=$AMOPTS" --include-deps";
 fi
 
-echo "Running aclocal -I macros"
-$ACLOCAL -I macros || giveup
-echo "Running autoheader"
-autoheader || giveup
-echo "Running libtoolize"
-$LIBTOOLIZE --force --copy || giveup
-echo "Running automake"
-$AUTOMAKE $AMFLAGS # || giveup
-echo "Running autoconf"
-autoconf || giveup
+LTOPTS="--force --copy"
+echo "* Running ${LIBTOOLIZE} (${LIBTOOLIZE_VER})"
+echo "\tOPTIONS = ${LTOPTS}"
+${LIBTOOLIZE} ${LTOPTS} || giveup
 
-echo "======================================"
-echo "Now you are ready to run './configure'"
-echo "======================================"
+echo "* Running $ACLOCAL (${ACLOCAL_VER})"
+${ACLOCAL} -I macros || giveup
+
+echo "* Running ${AUTOHEADER} (${AUTOCONF_VER})"
+${AUTOHEADER} || giveup
+
+echo "* Running ${AUTOMAKE} (${AUTOMAKE_VER})"
+echo "\tOPTIONS = ${AMOPTS}"
+${AUTOMAKE} ${AMOPTS} || giveup
+
+echo "* Running ${AUTOCONF} (${AUTOCONF_VER})"
+${AUTOCONF} || giveup
+
+if test -f "${PWD}/configure"; then
+    echo "======================================"
+    echo "Now you are ready to run './configure'"
+    echo "======================================"
+else
+    echo "  Failed to generate ./configure script!"
+    giveup
+fi
+
