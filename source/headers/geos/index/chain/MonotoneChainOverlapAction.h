53f4ca0a4943a34a0c334d3a9e7d2941d0d5553c
Sandro Santilli <strk@keybit.net>
2009-05-06 17:14:27 +0000

    Port info (to be worked on for heap allocations reduction)
    git-svn-id: http://svn.osgeo.org/geos/trunk@2478 5242fede-7e19-0410-aef8-94bd7d2200fb

--- source/headers/geos/index/chain/MonotoneChainOverlapAction.h
+++ source/headers/geos/index/chain/MonotoneChainOverlapAction.h
@@ -11,6 +11,10 @@
  * by the Free Software Foundation. 
  * See the COPYING file for more information.
  *
+ **********************************************************************
+ *
+ * Last port: index/chain/MonotoneChainOverlapAction.java rev. 1.6 (JTS-1.10)
+ *
  **********************************************************************/
 
 #ifndef GEOS_IDX_CHAIN_MONOTONECHAINOVERLAPACTION_H
