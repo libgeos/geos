79d07bf2576a4e3459d50ec2b6eab4c8779cafa8
Mateusz Loskot <mateusz@loskot.net>
2009-12-03 20:29:05 +0000

    Removed mysterious extra tokens (blanks) from geos/export.h
    git-svn-id: http://svn.osgeo.org/geos/trunk@2790 5242fede-7e19-0410-aef8-94bd7d2200fb

--- source/headers/geos/export.h
+++ source/headers/geos/export.h
@@ -1,17 +1,17 @@
 /**********************************************************************
- * $Id: export.h 2045 2008-03-17 20:18:24Z Ragi Burhum $
- *
- * GEOS - Geometry Engine Open Source
- * http://geos.refractions.net
- *
- * Copyright (C) 2009   Ragi Y. Burhum <ragi@burhum.com>
- *
- * This is free software; you can redistribute and/or modify it under
- * the terms of the GNU Lesser General Public Licence as published
- * by the Free Software Foundation. 
- * See the COPYING file for more information.
- *
- **********************************************************************/
+* $Id: export.h 2045 2008-03-17 20:18:24Z Ragi Burhum $
+*
+* GEOS - Geometry Engine Open Source
+* http://geos.refractions.net
+*
+* Copyright (C) 2009   Ragi Y. Burhum <ragi@burhum.com>
+*
+* This is free software; you can redistribute and/or modify it under
+* the terms of the GNU Lesser General Public Licence as published
+* by the Free Software Foundation. 
+* See the COPYING file for more information.
+*
+**********************************************************************/
 
 #ifndef GEOS_EXPORT_H
 #define GEOS_EXPORT_H
@@ -19,15 +19,16 @@
 #if defined(_MSC_VER) || defined(__CYGWIN__) || defined(__MINGW32__) || \
     defined( __BCPLUSPLUS__)  || defined( __MWERKS__)
 
-    #if defined(GEOS_DLL_EXPORT)
-        #define GEOS_DLL   __declspec(dllexport)
-    #elif defined(GEOS_DLL_IMPORT)
-        #define GEOS_DLL   __declspec(dllimport)
-    #else
-        #define GEOS_DLL
-    #endif
+#  if defined(GEOS_DLL_EXPORT)
+#    define GEOS_DLL   __declspec(dllexport)
+#  elif defined(GEOS_DLL_IMPORT)
+#    define GEOS_DLL   __declspec(dllimport)
+#  else
+#    define GEOS_DLL
+#  endif
 #else
-    #define GEOS_DLL
+#  define GEOS_DLL
 #endif
 
 #endif // GEOS_EXPORT_H
+
