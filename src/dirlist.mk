d67ac2be4eafdbd48b1da347a7923348d0b3216e
Mateusz Loskot <mateusz@loskot.net>
2010-04-07 13:22:56 +0000

    Updated src/dirlist.mk (Ticket 337)
    git-svn-id: http://svn.osgeo.org/geos/trunk@2967 5242fede-7e19-0410-aef8-94bd7d2200fb

--- src/dirlist.mk
+++ src/dirlist.mk
@@ -1,45 +1,45 @@
 # $Id$
 #
-# List of directories use by makefile.vc
+# List of directories use by makefile.vc to clean .obj files
 #
 
-GEOS_DIRLIST =  algorithm \
-                algorithm\distance \
-                algorithm\locate \
-                examples \
-                geom \
-                geom\util \
-                geom\prep \
-                geomgraph \
-                geomgraph\index \
-                headers \
-                headers\geos \
-                index \
-                index\bintree \
-                index\chain \
-                index\intervalrtree \
-                index\quadtree \
-                index\strtree \
-                index\sweepline \
-                io \
-		linearref \
-                noding \
-                noding\snapround \
-                operation \
-                operation\buffer \
-                operation\distance \
-                operation\linemerge \
-                operation\overlay \
-                operation\overlay\snap \
-                operation\overlay\validate \
-                operation\polygonize \
-                operation\predicate \
-                operation\relate \
-                operation\union \
-                operation\valid \
-                planargraph \
-                planargraph\algorithm \
-                precision \
-		simplify \
-                util
-
+GEOS_DIRLIST =  \
+	algorithm \
+	algorithm\distance \
+	algorithm\locate \
+	examples \
+	geom \
+	geom\util \
+	geom\prep \
+	geomgraph \
+	geomgraph\index \
+	headers \
+	headers\geos \
+	index \
+	index\bintree \
+	index\chain \
+	index\intervalrtree \
+	index\quadtree \
+	index\strtree \
+	index\sweepline \
+	io \
+	linearref \
+	noding \
+	noding\snapround \
+	operation \
+	operation\buffer \
+	operation\distance \
+	operation\linemerge \
+	operation\overlay \
+	operation\overlay\snap \
+	operation\overlay\validate \
+	operation\polygonize \
+	operation\predicate \
+	operation\relate \
+	operation\union \
+	operation\valid \
+	planargraph \
+	planargraph\algorithm \
+	precision \
+	simplify \
+	util
