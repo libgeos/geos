a8886cc45651db21403b5a6bacc7853b20e530ed
Mateusz Loskot <mateusz@loskot.net>
2010-10-10 21:28:29 +0000

    Disable inline.obj if GEOS_INLINE defined while building with Visual C++
    git-svn-id: http://svn.osgeo.org/geos/trunk@3109 5242fede-7e19-0410-aef8-94bd7d2200fb

--- src/inlines.cpp
+++ src/inlines.cpp
@@ -23,17 +23,18 @@
 // Otherwise we'll end up with duplicated symbols
 #ifdef GEOS_INLINE
 
+// If using Visual C++ with GEOS_INLINE, do not build inline.obj
+// otherwise linker will complain "multiple definition" errors.
 // If using MingW with GEOS_INLINE to build a DLL then MingW's gcc
 // has already generated the stubs for the contents of this file. 
 // Hence we need to supress it to avoid "multiple definition" errors
 // during the final link phase
-#if ! defined(__MINGW32__) || defined(__MINGW32__) && !defined(DLL_EXPORT) 
+#if !defined(_MSC_VER) && (!defined(__MINGW32__) || defined(__MINGW32__) && !defined(DLL_EXPORT))
 
 // If using cygwin then we suppress the "multiple definition" errors by
 // ignoring this section completely; the cygwin linker seems to handle
 // the stubs correctly at link time by itself
-#if ! defined(__CYGWIN__)
-
+#if !defined(__CYGWIN__)
 
 // Undefine GEOS_INLINE so that .inl files
 // will be ready for an implementation file
