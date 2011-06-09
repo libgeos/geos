c56cae60c78709f8e15d886c9308536f1f1ffc73
Sandro Santilli <strk@keybit.net>
2011-06-09 15:28:14 +0000

    When checking for validity, do not abort if every vertex of an hole is also a vertex of another one. Fixes bug #449, including regress testing.
    git-svn-id: http://svn.osgeo.org/geos/trunk@3392 5242fede-7e19-0410-aef8-94bd7d2200fb

--- src/operation/valid/IndexedNestedRingTester.cpp
+++ src/operation/valid/IndexedNestedRingTester.cpp
@@ -50,7 +50,6 @@ IndexedNestedRingTester::isNonNested()
 		for (size_t j=0, jn=results.size(); j<jn; ++j)
 		{
 			const geom::LinearRing* searchRing = static_cast<const geom::LinearRing*>(results[j]);
-			const geom::CoordinateSequence *searchRingPts=searchRing->getCoordinatesRO();
 
 			if (innerRing==searchRing)
 				continue;
@@ -61,15 +60,20 @@ IndexedNestedRingTester::isNonNested()
 				continue;
 			}
 
-                        const geom::Coordinate *innerRingPt =
+			const geom::Coordinate *innerRingPt =
 				IsValidOp::findPtNotNode(innerRingPts,
 							 searchRing,
 							 graph);
 
-                        // Unable to find a ring point not a node of
-			// the search ring
-			assert(innerRingPt!=NULL);
+			// Unable to find a ring point not a node of
+			// the search ring. Can happen, see
+			// http://trac.osgeo.org/geos/ticket/449
+			// for examples.
+			if ( ! innerRingPt ) {
+			  continue;
+			}
 
+			const geom::CoordinateSequence *searchRingPts=searchRing->getCoordinatesRO();
 			bool isInside = algorithm::CGAlgorithms::isPointInRing(	
 					*innerRingPt, searchRingPts);
 
