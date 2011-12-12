428c75a3b86b493983249c7b15775400a6a82fe6
Howard Butler <hobu.inc@gmail.com>
2011-12-12 15:09:26 +0000

    apply patch for #500 to fix clang's complaint about a missing default initialization
    git-svn-id: http://svn.osgeo.org/geos/trunk@3546 5242fede-7e19-0410-aef8-94bd7d2200fb

--- src/operation/overlay/OverlayNodeFactory.cpp
+++ src/operation/overlay/OverlayNodeFactory.cpp
@@ -27,8 +27,6 @@ namespace geos {
 namespace operation { // geos.operation
 namespace overlay { // geos.operation.overlay
 
-static const OverlayNodeFactory onf;
-
 Node*
 OverlayNodeFactory::createNode(const geom::Coordinate &coord) const
 {
@@ -38,6 +36,7 @@ OverlayNodeFactory::createNode(const geom::Coordinate &coord) const
 const NodeFactory &
 OverlayNodeFactory::instance() 
 {
+  static OverlayNodeFactory onf;
 	return onf;
 }
 
