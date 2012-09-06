bba8dfc1aba662e066c45f11e70e262a4e1abc7d
Sandro Santilli <strk@keybit.net>
2012-09-06 18:15:17 +0000

    Use version_compare to compare versions...
    git-svn-id: http://svn.osgeo.org/geos/branches/3.3@3707 5242fede-7e19-0410-aef8-94bd7d2200fb

--- php/test/test.php
+++ php/test/test.php
@@ -5,9 +5,8 @@
 
 dl("geos.so");
 
-$phpunit_versions = explode('.', PHPUnit_Runner_Version::id());
-$phpunit_version = $phpunit_versions[0] . $phpunit_versions[1];
-if ( $phpunit_version < 36 ) require_once 'PHPUnit/Framework.php';
+if ( version_compare(PHPUnit_Runner_Version::id(), '3.6') < 0 )
+    require_once 'PHPUnit/Framework.php';
 
 
 class test extends PHPUnit_Framework_TestCase
@@ -15,8 +14,9 @@ class test extends PHPUnit_Framework_TestCase
     # This method override is needed to support phpunit < 3.5 (Ubuntu 10.04 ships 3.4)
     static public function assertType($x, $y)
     {
-        global $phpunit_version;
-        if ( $phpunit_version < 35 ) return PHPUnit_Framework_TestCase::assertType($x, $y);
+        #global $phpunit_version;
+        if ( version_compare(PHPUnit_Runner_Version::id(), '3.5') < 0 )
+            return PHPUnit_Framework_TestCase::assertType($x, $y);
         else return PHPUnit_Framework_TestCase::assertInternalType($x, $y);
     }
 
