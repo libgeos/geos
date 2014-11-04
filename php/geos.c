422fa5bd3bbe6810b63be44a4cc003bfd5111590
Sandro Santilli <strk@keybit.net>
2014-11-04 18:15:32 +0000

    Fix build of PHP bindings with PHP < 5.3.99 (#709)
    Tested with php 5.3.2
    Thanks KayMadejski for the report
    
    git-svn-id: http://svn.osgeo.org/geos/trunk@4027 5242fede-7e19-0410-aef8-94bd7d2200fb

--- php/geos.c
+++ php/geos.c
@@ -171,7 +171,7 @@ Gen_create_obj (zend_class_entry *type,
     zend_hash_init(obj->std.properties, 0, NULL, ZVAL_PTR_DTOR, 0);
 #if PHP_VERSION_ID < 50399 
     zend_hash_copy(obj->std.properties, &type->default_properties,
-        (copy_ctor_func_t)zval_add_ref, (void *)&tmp, sizeof(zval *));
+        (copy_ctor_func_t)zval_add_ref, NULL, sizeof(zval *));
 #else
     object_properties_init(&(obj->std), type);
 #endif
