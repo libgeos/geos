343da94a9b8b2d717ec9e7b3856fbd678e87a2b5
Mateusz Loskot <mateusz@loskot.net>
2017-03-27 00:30:03 +0000

    First stab at tests/uni2 with Catch-based tests
    Add Point test.
    Add CMake configuration.
    Build unit2 tests only if GEOS_ENABLE_TESTS_UNIT2_ONLY=ON.
    Add automatic download of Catch header.
    Require C++11 to build Catch-based tests
    
    git-svn-id: http://svn.osgeo.org/geos/trunk@4378 5242fede-7e19-0410-aef8-94bd7d2200fb

---
+++ tests/unit2/geom/PointTest.cpp
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// geos::geom::Point class test
+//
+// Copyright (C) 2017 Mateusz Loskot <mateusz@loskot.net>
+//
+// This is free software; you can redistribute and/or modify it under
+// the terms of the GNU Lesser General Public Licence as published
+// by the Free Software Foundation. 
+// See the COPYING file for more information.
+////////////////////////////////////////////////////////////////////////////////
+#include <catch.hpp>
+
+#include <geos/geom/GeometryFactory.h>
+#include <geos/geom/Point.h>
+using namespace geos::geom;
+
+TEST_CASE("Point")
+{
+	GeometryFactory::unique_ptr factory{GeometryFactory::create()};
+
+	SECTION("empty")
+	{
+		std::unique_ptr<Point> p{factory->createPoint()};
+		REQUIRE(p->isEmpty());
+	}
+}
