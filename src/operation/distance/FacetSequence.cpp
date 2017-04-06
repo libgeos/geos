2e54be24bffc733989a8e9076f85ba3727f1800e
Daniel Baston <dbaston@maponics.com>
2017-04-06 08:52:19 -0400

    Fix invalid read in FacetSequence

--- src/operation/distance/FacetSequence.cpp
+++ src/operation/distance/FacetSequence.cpp
@@ -44,14 +44,14 @@ double FacetSequence::distance(const FacetSequence & facetSeq) const {
 
     if (isPointThis && isPointOther) {
         Coordinate pt = pts->getAt(start);
-        Coordinate seqPt = facetSeq.pts->getAt(start);
+        Coordinate seqPt = facetSeq.pts->getAt(facetSeq.start);
         return pt.distance(seqPt);
 
     } else if (isPointThis) {
         Coordinate pt = pts->getAt(start);
         return computePointLineDistance(pt, facetSeq);
     } else if (isPointOther) {
-        Coordinate seqPt = facetSeq.pts->getAt(start);
+        Coordinate seqPt = facetSeq.pts->getAt(facetSeq.start);
         return computePointLineDistance(seqPt, *this);
     }
 
