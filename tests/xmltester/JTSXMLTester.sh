88fbd4d41b0e3ac92bb170883f8153e0b10ffb6e
Sandro Santilli <strk@kbt.io>
2017-07-14 12:50:21 +0200

    Add copyright header

--- tests/xmltester/JTSXMLTester.sh
+++ tests/xmltester/JTSXMLTester.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
+######################################################################
+#
+# GEOS - Geometry Engine Open Source
+# http://geos.osgeo.org
+#
+# Copyright (C) 2010-2017 Sandro Santilli <strk@kbt.io>
+# Copyright (C) 2010-2012 Martin Davis <mtnclimb@gmail.com>
+#
+# This is free software; you can redistribute and/or modify it under
+# the terms of the GNU Lesser General Public Licence as published
+# by the Free Software Foundation.
+# See the COPYING file for more information.
+#
+######################################################################
+
 if test "x$JTS_LIB_DIR" = "x"; then
-	JTS_LIB_DIR="$HOME/.m2/repository/org/locationtech/jts"
+  JTS_LIB_DIR="$HOME/.m2/repository/org/locationtech/jts"
 fi
 
 #---------------------------------#
