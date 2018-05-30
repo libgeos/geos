9c1c5e09b08bf42d3e97836a09017068e602b6f3
Daniel Baston <dbaston@gmail.com>
2018-05-29 21:46:34 -0400

    Add GEOSSegmentIntersection to CAPI
    Closes #873
    Closes https://github.com/OSGeo/geos/pull/101

---
+++ tests/unit/capi/GEOSSegmentIntersectionTest.cpp
@@ -0,0 +1,83 @@
+//
+// Test Suite for C-API GEOSSegmentIntersection
+#include <tut/tut.hpp>
+// geos
+#include <geos_c.h>
+// std
+#include <cstdarg>
+#include <cstdio>
+#include <cstdlib>
+
+namespace tut
+{
+    //
+    // Test Group
+    //
+
+    // Common data used in test cases.
+    struct test_capigeossegmentintersection
+    {
+        static void notice(const char *fmt, ...)
+        {
+            std::fprintf( stdout, "NOTICE: ");
+
+            va_list ap;
+            va_start(ap, fmt);
+            std::vfprintf(stdout, fmt, ap);
+            va_end(ap);
+
+            std::fprintf(stdout, "\n");
+        }
+
+        test_capigeossegmentintersection()
+        {
+            initGEOS(notice, notice);
+        }
+
+        ~test_capigeossegmentintersection()
+        {
+            finishGEOS();
+        }
+    };
+
+    typedef test_group<test_capigeossegmentintersection> group;
+    typedef group::object object;
+
+    group test_capigeossegmentintersection_group("capi::GEOSSegmentIntersection");
+
+    //
+    // Test Cases
+    //
+    template<>
+    template<>
+    void object::test<1>()
+    {
+        // plain old intersection
+        int result;
+        double x,y;
+
+        result = GEOSSegmentIntersection(0, 0, 10, 10,
+                                         8, 0, 8, 10,
+                                         &x, &y);
+
+        ensure_equals(result, 1);
+        ensure_equals(x, 8);
+        ensure_equals(y, 8);
+    }
+
+    template<>
+    template<>
+    void object::test<2>()
+    {
+        // no intersection
+        int result;
+        double x,y;
+
+        result = GEOSSegmentIntersection(0, 0, 10, 10,
+                                         8, 0, 8,  2,
+                                         &x, &y);
+
+        ensure_equals(result, -1);
+    }
+
+} // namespace tut
