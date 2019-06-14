36d1c69636062de78688cb7e995eccbc27785287
Daniel Baston <dbaston@gmail.com>
2019-06-13 21:21:52 -0400

    Add test case for #850

--- tests/unit/algorithm/ConvexHullTest.cpp
+++ tests/unit/algorithm/ConvexHullTest.cpp
@@ -13,6 +13,7 @@
 #include <geos/geom/Geometry.h>
 #include <geos/geom/GeometryFactory.h>
 #include <geos/geom/PrecisionModel.h>
+#include <geos/io/WKBReader.h>
 #include <geos/io/WKTReader.h>
 // std
 #include <sstream>
@@ -188,6 +189,33 @@ void object::test<7>
 
     geom_ = geom->convexHull();
     ensure(convexHull->equalsExact(geom_.get()));
+}
+
+template<>
+template<>
+void object::test<8>
+()
+{
+    // Test case from https://trac.osgeo.org/geos/ticket/850
+    geos::io::WKBReader r;
+    std::stringstream wkb("01040000001100000001010000002bd3a24002bcb0417ff59d2051e25c4101010000003"
+                          "aebcec70a8b3cbfdb123fe713a2e8be0101000000afa0bb8638b770bf7fc1d77d0dda1c"
+                          "bf01010000009519cb944ce070bf1a46cd7df4201dbf010100000079444b4cd1937cbfa"
+                          "6ca29ada6a928bf010100000083323f09e16c7cbfd36d07ee0b8828bf01010000009081"
+                          "b8f066967ebf915fbc9ebe652abf0101000000134cf280633bc1bf37b754972dbe6dbf0"
+                          "101000000ea992c094df585bf1bbabc8a42f332bf0101000000c0a13c7fb31186bf9af7"
+                          "b10cc50b33bf0101000000a0bba15a0a7188bf8fba7870e91735bf01010000000fc8701"
+                          "903db93bf93bdbe93b52241bf01010000007701a73b29cc90bfb770bc3732fe3cbf0101"
+                          "00000036fa45b75b8b8cbf1cfca5bf59a238bf0101000000a54e773f7f287ebf910d462"
+                          "1e5062abf01010000004b5b5dc4196f55bfa51f0579717f02bf01010000007e54948951"
+                          "3a5fbfa57bacea34f30abf");
+    Geometry::Ptr geom(r.readHEX(wkb));
+
+    ensure(nullptr != geom);
+
+    auto result = geom->convexHull();
+
+    ensure(result != nullptr); // No crash!
 }
 
 } // namespace tut
