60583394172397a47e444443175d06d7dbb17f1c
Paul Ramsey <pramsey@cleverelephant.ca>
2019-09-26 10:01:13 -0700

    Expose MinimumBoundingCircle to CAPI Closes #735

--- capi/geos_ts_c.cpp
+++ capi/geos_ts_c.cpp
@@ -44,7 +44,7 @@
 #include <geos/io/WKTWriter.h>
 #include <geos/io/WKBWriter.h>
 #include <geos/algorithm/BoundaryNodeRule.h>
-#include <geos/algorithm/Orientation.h>
+#include <geos/algorithm/MinimumBoundingCircle.h>
 #include <geos/algorithm/MinimumDiameter.h>
 #include <geos/algorithm/Orientation.h>
 #include <geos/algorithm/distance/DiscreteHausdorffDistance.h>
@@ -2988,6 +2988,44 @@ extern "C" {
                 return gf->createPoint().release();
             }
             return ret;
+        }
+        catch(const std::exception& e) {
+            handle->ERROR_MESSAGE("%s", e.what());
+        }
+        catch(...) {
+            handle->ERROR_MESSAGE("Unknown exception thrown");
+        }
+
+        return NULL;
+    }
+
+    Geometry*
+    GEOSMinimumBoundingCircle_r(GEOSContextHandle_t extHandle, const Geometry* g,
+        double* radius, Geometry** center)
+    {
+        if(0 == extHandle) {
+            return NULL;
+        }
+
+        GEOSContextHandleInternal_t* handle = 0;
+        handle = reinterpret_cast<GEOSContextHandleInternal_t*>(extHandle);
+        if(0 == handle->initialized) {
+            return NULL;
+        }
+
+        try {
+            geos::algorithm::MinimumBoundingCircle mc(g);
+            std::unique_ptr<Geometry> ret = mc.getCircle();
+            const GeometryFactory* gf = handle->geomFactory;
+            if(!ret) {
+                if (center) *center = NULL;
+                if (radius) *radius = 0.0;
+                return gf->createPolygon().release();
+            }
+            if (center) *center = static_cast<Geometry*>(gf->createPoint(mc.getCentre()));
+            if (radius) *radius = mc.getRadius();
+            ret->setSRID(g->getSRID());
+            return ret.release();
         }
         catch(const std::exception& e) {
             handle->ERROR_MESSAGE("%s", e.what());
