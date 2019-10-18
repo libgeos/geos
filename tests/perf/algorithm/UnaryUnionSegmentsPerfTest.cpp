b363b501d5ceeecee3eba51277adecf975308174
Daniel Baston <dbaston@gmail.com>
2019-10-18 18:16:49 -0400

    Backport perf tests to 3.7

---
+++ tests/perf/algorithm/UnaryUnionSegmentsPerfTest.cpp
@@ -0,0 +1,72 @@
+/**********************************************************************
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.osgeo.org
+ *
+ * Copyright (C) 2019 Daniel Baston <dbaston@gmail.com>
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation.
+ * See the COPYING file for more information.
+ *
+ **********************************************************************/
+
+#include <geos/triangulate/DelaunayTriangulationBuilder.h>
+#include <geos/triangulate/VoronoiDiagramBuilder.h>
+#include <geos/geom/CoordinateArraySequence.h>
+#include <geos/geom/GeometryFactory.h>
+#include <geos/geom/LineString.h>
+#include <geos/profiler.h>
+
+#include <algorithm>
+#include <random>
+#include <vector>
+#include <memory>
+
+class SegmentUnaryUnionPerfTest {
+
+public:
+    void test(size_t num_lines) {
+        using namespace geos::geom;
+
+        std::default_random_engine e(12345);
+        std::uniform_real_distribution<> dis(0, 100);
+
+        std::vector<Geometry*>* lines = new std::vector<Geometry*>();
+
+        for (size_t i = 0; i < num_lines; i++) {
+            CoordinateArraySequence* cas = new CoordinateArraySequence(2, 2);
+            cas->setAt(Coordinate(dis(e), dis(e)), 0);
+            cas->setAt(Coordinate(dis(e), dis(e)), 1);
+
+            Geometry* line = gfact->createLineString(cas);
+            lines->push_back(line);
+        }
+
+        auto g = gfact->createMultiLineString(lines);
+
+        auto sw = profiler->get("union");
+        sw->start();
+
+        g->Union();
+
+        sw->stop();
+
+        std::cout << *sw << std::endl;
+    }
+private:
+    decltype(geos::geom::GeometryFactory::create()) gfact = geos::geom::GeometryFactory::create();
+    geos::util::Profiler* profiler = geos::util::Profiler::instance();
+};
+
+int main(int argc, char** argv) {
+    SegmentUnaryUnionPerfTest tester;
+
+    auto num_lines = std::atol(argv[1]);
+    auto num_reps = argc > 2 ? std::atol(argv[2]) : 1;
+
+    for (int i = 0; i < num_reps; i++) {
+        tester.test(num_lines);
+    }
+}
---
+++ tests/perf/capi/GEOSPreparedContainsPerfTest.cpp
@@ -0,0 +1,115 @@
+/**********************************************************************
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.osgeo.org
+ *
+ * Copyright (C) 2019 Daniel Baston <dbaston@gmail.com>
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation.
+ * See the COPYING file for more information.
+ *
+ **********************************************************************/
+
+#include <geos/profiler.h>
+#include <geos_c.h>
+
+#include <geos/geom/Coordinate.h>
+#include <geos/geom/GeometryFactory.h>
+#include <geos/geom/Polygon.h>
+#include <geos/geom/util/SineStarFactory.h>
+
+#include <algorithm>
+#include <random>
+#include <vector>
+#include <memory>
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+class GEOSPreparedContainsPerfTest {
+
+public:
+    void test(const GEOSGeometry* g, size_t num_points) {
+        using namespace geos::geom;
+
+        double xmin, xmax, ymin, ymax;
+
+        GEOSGeom_getXMin(g, &xmin);
+        GEOSGeom_getXMax(g, &xmax);
+        GEOSGeom_getYMin(g, &ymin);
+        GEOSGeom_getYMax(g, &ymax);
+
+        std::default_random_engine e(12345);
+        std::uniform_real_distribution<> xdist(xmin, xmax);
+        std::uniform_real_distribution<> ydist(ymin, ymax);
+
+        std::vector<Coordinate> coords(num_points);
+        std::generate(coords.begin(), coords.end(), [&xdist, &ydist, &e]() {
+            return Coordinate(xdist(e), ydist(e));
+        });
+
+        geos::util::Profile sw("GEOSPreparedContains");
+        sw.start();
+
+        size_t hits = 0;
+        auto prep = GEOSPrepare(g);
+        for (const auto& c : coords) {
+            auto seq = GEOSCoordSeq_create(1, 2);
+            GEOSCoordSeq_setX(seq, 0, c.x);
+            GEOSCoordSeq_setY(seq, 0, c.y);
+
+            auto pt = GEOSGeom_createPoint(seq);
+
+            if (GEOSPreparedContains(prep, pt)) {
+                hits++;
+            }
+
+            GEOSGeom_destroy(pt);
+        }
+
+        GEOSPreparedGeom_destroy(prep);
+
+        sw.stop();
+
+        std::cout << sw.name << ": " << hits << " hits from " << num_points << " points in " <<  sw.getTot() << std::endl;
+
+    }
+};
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        std::cout << "perf_geospreparedcontins performs a specified number of point-in-polygon tests" << std::endl;
+        std::cout << "on randomly generated points from the bounding box of a single geometry provided" << std::endl;
+        std::cout << "in a file as WKT." << std::endl;
+        std::cout << std::endl;
+        std::cout << "Usage: perf_geospreparedcontins [wktfile] [n]" << std::endl;
+        return 0;
+    }
+
+    GEOSPreparedContainsPerfTest tester;
+
+    int n = std::atoi(argv[2]);
+    std::cout << "Performing " << n << " point-in-polygon tests." << std::endl;
+
+    std::string fname{argv[1]};
+    std::cout << "Reading shape from " << fname << std::endl;
+
+    std::ifstream f(fname);
+    std::stringstream buff;
+    buff << f.rdbuf();
+    f.close();
+
+    std::string wkt = buff.str();
+    buff.clear();
+
+    initGEOS(nullptr, nullptr);
+    GEOSGeometry* g = GEOSGeomFromWKT(wkt.c_str());
+    wkt.clear();
+
+    tester.test(g, n);
+
+    GEOSGeom_destroy(g);
+}
