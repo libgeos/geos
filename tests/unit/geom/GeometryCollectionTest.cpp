0698537490c45e0e45dca506ede6b02a556a5661
doskabouter <paul.doskabouter@gmail.com>
2019-10-28 16:11:48 +0100

    Added unit-test for GeometryCollection::envelopeIntersects

--- tests/unit/geom/GeometryCollectionTest.cpp
+++ tests/unit/geom/GeometryCollectionTest.cpp
@@ -143,4 +143,43 @@ void object::test<6>
     ensure(!gc->isDimensionStrict(geos::geom::Dimension::A));
 }
 
+// test envelopeIntersects
+template<>
+template<>
+void object::test<7>
+()
+{
+    //polygon-polygon
+    auto geom1 = readWKT("POLYGON((0 0,100 0, 100 100, 0 100,0 0))");
+    auto geom2 = readWKT("POLYGON((200 0,300 0, 300 100, 200 100,200 0))");
+    ensure(!geos::geom::GeometryCollection::envelopeIntersects(geom1.get(), geom2.get()));
+
+    geom1 = readWKT("POLYGON((0 0,100 0, 100 100, 0 100,0 0))");
+    geom2 = readWKT("POLYGON((100 0,200 0, 200 100, 100 100,100 0))");
+    ensure(geos::geom::GeometryCollection::envelopeIntersects(geom1.get(), geom2.get()));
+
+    //multipolygon-polygon, and polygon-multipolygon
+    geom1 = readWKT("MULTIPOLYGON(((0 0,100 0, 100 100, 0 100,0 0)),((400 0,500 0, 500 100, 400 100,400 0)))");
+    geom2 = readWKT("POLYGON((200 0,300 0, 300 100, 200 100,200 0))");
+    ensure(!geos::geom::GeometryCollection::envelopeIntersects(geom1.get(), geom2.get()));
+    ensure(!geos::geom::GeometryCollection::envelopeIntersects(geom1.get(), geom2.get()));
+
+    geom1 = readWKT("MULTIPOLYGON(((0 0,100 0, 100 100, 0 100,0 0)),((400 0,500 0, 500 100, 400 100,400 0)))");
+    geom2 = readWKT("POLYGON((300 0,400 0, 400 100, 300 100,300 0))");
+    ensure(geos::geom::GeometryCollection::envelopeIntersects(geom1.get(), geom2.get()));
+    ensure(geos::geom::GeometryCollection::envelopeIntersects(geom1.get(), geom2.get()));
+
+    //multipolygon-multipolygon
+    geom1 = readWKT("MULTIPOLYGON(((0 0,100 0, 100 100, 0 100,0 0)),((400 0,500 0, 500 100, 400 100,400 0)))");
+    geom2 = readWKT("MULTIPOLYGON(((200 0,300 0, 300 100, 200 100,200 0)),((600 0,700 0, 700 100, 600 100,600 0)))");
+    //technically these envelopes don't intersect, but to calculate that would require a O(NxM) algorithm
+    ensure(geos::geom::GeometryCollection::envelopeIntersects(geom1.get(), geom2.get()));
+    ensure(geos::geom::GeometryCollection::envelopeIntersects(geom1.get(), geom2.get()));
+
+    geom1 = readWKT("MULTIPOLYGON(((0 0,100 0, 100 100, 0 100,0 0)),((400 0,500 0, 500 100, 400 100,400 0)))");
+    geom2 = readWKT("MULTIPOLYGON(((300 0,400 0, 400 100, 300 100,300 0)),((600 0,700 0, 700 100, 600 100,600 0)))");
+    ensure(geos::geom::GeometryCollection::envelopeIntersects(geom1.get(), geom2.get()));
+    ensure(geos::geom::GeometryCollection::envelopeIntersects(geom1.get(), geom2.get()));
+}
+
 } // namespace tut
