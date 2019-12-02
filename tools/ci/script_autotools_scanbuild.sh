ec16c426eb355d01ef175e338213de6e5df291c3
Raúl Marín <git@rmr.ninja>
2019-12-02 19:25:28 +0100

    Autotools: Add std=c++11 automatically to the builds

--- tools/ci/script_autotools_scanbuild.sh
+++ tools/ci/script_autotools_scanbuild.sh
@@ -21,5 +21,5 @@ fi
 cd ${TRAVIS_BUILD_DIR}
 ./autogen.sh
 cd -
-${TRAVIS_BUILD_DIR}/configure CC=clang CXX=clang++ CXXFLAGS="-std=c++11"
+${TRAVIS_BUILD_DIR}/configure CC=clang CXX=clang++
 scan-build --status-bugs make -j2
