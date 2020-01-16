b0f91afd8f4d1a64a018bb492c4f0d311405642b
azhi <azhi@users.noreply.github.com>
2020-01-16 16:13:02 +0300

    remove whitespace at the end of capi version

--- capi/geos_ts_c.cpp
+++ capi/geos_ts_c.cpp
@@ -1931,7 +1931,7 @@ extern "C" {
     const char* GEOSversion()
     {
         static char version[256];
-        sprintf(version, "%s ", GEOS_CAPI_VERSION);
+        sprintf(version, "%s", GEOS_CAPI_VERSION);
         return version;
     }
 
