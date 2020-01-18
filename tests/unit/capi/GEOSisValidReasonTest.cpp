bfa50020fdf725fd9e6b96278f3af645cda4769f
Daniel Baston <dbaston@gmail.com>
2020-01-17 22:47:18 -0500

    Add some CAPI tests

---
+++ tests/unit/capi/GEOSisValidReasonTest.cpp
@@ -0,0 +1,43 @@
+#include <tut/tut.hpp>
+// geos
+#include <geos_c.h>
+
+#include "capi_test_utils.h"
+
+namespace tut {
+//
+// Test Group
+//
+
+struct test_geosisvalid_data : public capitest::test_handlers {};
+
+typedef test_group<test_geosisvalid_data> group;
+typedef group::object object;
+
+group test_geosisvalid("capi::GEOSisValid");
+
+template<>
+template<>
+void object::test<1>()
+{
+    GEOSGeometry* input = GEOSGeomFromWKT("LINESTRING (1 2, 4 5, 9 -2)");
+
+    ensure_equals(1, GEOSisValid(input));
+
+    GEOSGeom_destroy(input);
+}
+
+template<>
+template<>
+void object::test<2>()
+{
+    GEOSGeometry* input = GEOSGeomFromWKT("POLYGON ((0 0, 1 0, 0 1, 1 1, 0 0))");
+
+    ensure_equals(0, GEOSisValid(input));
+
+    GEOSGeom_destroy(input);
+}
+
+
+} // namespace tut
+
