73a9e4db00b456fbb8bc76a0f5e331970e0d3ab1
Jonathan Adams <jd.adams16@gmail.com>
2020-03-30 00:36:32 -0500

    Start adding structure based on MinimumBoundingCircle.

---
+++ include/geos/algorithm/MaximumInscribedCircle.h
@@ -0,0 +1,105 @@
+/**********************************************************************
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.osgeo.org
+ *
+ * Copyright (C) 2019 Jonathan Adams <jd.adams16@gmail.com>
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation.
+ * See the COPYING file for more information.
+ *
+ **********************************************************************/
+
+#ifndef GEOS_ALGORITHM_MAXIMUMINSCRIBEDCIRCLE_H
+#define GEOS_ALGORITHM_MAXIMUMINSCRIBEDCIRCLE_H
+
+#include <geos/geom/Coordinate.h>
+#include <geos/geom/CoordinateSequence.h>
+#include <geos/geom/Geometry.h>
+#include <geos/geom/MultiPolygon.h>
+#include <geos/geom/Point.h>
+#include <geos/geom/Polygon.h>
+
+namespace geos {
+namespace algorithm { // geos::algorithm
+
+class GEOS_DLL MaximumInscribedCircle {
+
+    private:
+
+        // member variables
+        const geom::Geometry* inputGeom;
+        const int* inputNumSegments;
+        std::vector<geom::Coordinate> sites;
+        const unsigned int* inputNumSegments;
+        std::vector<Coordinate> sites;
+        std::vector<Coordinate> voronoiVertices;
+        geom::Coordinate center;
+        double radius;
+        double xmin, xmax, ymin, ymax;
+
+        void addRingSites(const LineString* ring);
+        void compute();
+        void computeSites(geom::Polygon& poly);
+        void computeVoronoiVertices();
+
+    public:
+
+        MaximumInscribedCircle(const geom::Geometry* geom, const int* numSegments):
+            inputGeom(nullptr),
+            inputNumSegments(0),
+            radius(0.0)
+        {
+            inputGeom = geom;
+            inputNumSegments = numSegments;
+            center.setNull();
+        }
+
+        ~MaximumInscribedCircle() {}; // This is a destructor and is required to free resources
+
+        /**
+         * Gets a geometry which represents the Maximum Inscribed Circle.
+         * If the input is degenerate (empty or a single unique point),
+         * this method will return an empty geometry or a single Point geometry.
+         * If the input is a Polygon, it will return the approximation of the
+         * Maximum Inscribed Circle for the given polygon. If the input is a
+         * MultiPolygon, then it will return the approximation of the Maximum
+         * Inscribed Circle for the polygon with the largest area. In all other
+         * cases an empty geometry will be returned.
+         *
+         * If the input is a Geometry Collection, then each geometry will be
+         * run through the algorithm and a Geometry Collection of all inscribed
+         * circles will be returned. Otherwise, a Polygon will be returned which
+         * approximates the Maximum Inscribed Circle.
+         *
+         * (Note that because this algorithm only approximates the medial axis
+         * via Voronoi, the result will not necessarily be the true Maximum
+         * Inscribed Circle.)
+         *
+         * @return A Geometry representing the Maximum Inscribed Circle.
+         */
+        std::unique_ptr<geom::Geometry> getCircle();
+
+        /**
+         * Gets the center point of the computed Maximum Inscribed Circle.
+         *
+         * @return the center point of the Maximum Incribed Circle
+         * @return null if the input is empty
+         */
+        geom::Coordinate getCenter();
+
+        /**
+         * Gets the radius of the computed Maximum Inscribed Circle.
+         *
+         * @return the radius of the Maximum Inscribed Circle
+         */
+        double getRadius();
+
+};
+
+} // namespace geos::algorithm
+} // namespace geos
+
+#endif // GEOS_ALGORITHM_MAXIMUMINSCRIBEDCIRCLE_H
---
+++ src/algorithm/MaximumInscribedCircle.cpp
@@ -0,0 +1,186 @@
+/**********************************************************************
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.osgeo.org
+ *
+ * Copyright (C) 2019 Jonathan Adams <jd.adams16@gmail.com>
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation.
+ * See the COPYING file for more information.
+ *
+ **********************************************************************/
+
+#include <geos/algorithm/MaximumInscribedCircle.h>
+#include <geos/geom/Coordinate.h>
+#include <geos/geom/CoordinateSequenceFactory.h>
+#include <geos/geom/Geometry.h>
+#include <geos/geom/GeometryFactory.h>
+#include <geos/geom/LineString.h>
+#include <geos/geom/Point.h>
+#include <geos/geom/Polygon.h>
+#include <geos/triangulate/VoronoiDiagramBuilder.h>
+#include <geos/util/UniqueCoordinateArrayFilter.h>
+
+#include <memory> // for unique_ptr
+#include <typeinfo>
+#include <vector>
+
+using namespace geos::geom;
+using namespace geos::triangulate;
+using namespace geos::util;
+
+namespace geos {
+namespace algorithm { // geos.algorithm
+
+/*public*/
+std::unique_ptr<Geometry>
+MaximumInscribedCircle::getCircle()
+{
+    // handle degenerate or trivial cases
+    if(inputGeom->isEmpty()) {
+        return std::unique_ptr<Geometry>(inputGeom->getFactory()->createPolygon());
+    } else if(inputGeom->getNumPoints() == 1) {
+        return std::unique_ptr<Geometry>(inputGeom->clone());
+    }
+    compute();
+}
+
+/*private*/
+void
+MaximumInscribedCircle::compute()
+{
+    // handle degenerate or trivial cases
+    if(inputGeom->isEmpty()) {
+        return;
+    }
+    if(inputGeom->getNumPoints() == 1) {
+        return;
+    }
+    // computeSites();
+}
+
+/*private*/
+void
+MaximumInscribedCircle::computeSites(geom::Polygon& poly)
+{
+    const LineString* exterior = poly.getExteriorRing();
+    const LineString* interior;
+
+    // Initialize bounding box
+    xmin = exterior->getStartPoint()->getX();
+    xmax = xmin;
+    ymin = exterior->getStartPoint()->getY();
+    ymax = ymin;
+
+    addRingSites(exterior);
+
+    for(unsigned int i = 0; i < poly.getNumInteriorRing(); i++) {
+        interior = poly.getInteriorRingN(i);
+        addRingSites(interior);
+    }
+}
+
+/**
+* This function implements the majority of GeoJSONUtils.sites from the
+* nodeJS module max-inscribed-circle (npmjs.com/package/max-inscribed-circle).
+* It takes a ring, and n-sects each segment of the ring, where n is the
+* number of segments MaximumInscribedCircle has been called with. It then
+* adds all of these n-sect coordinates into an array of coorinates that
+* will eventually be converted to a CoordinateSequence and used to call
+* Voronoi.
+*
+* This function also updates the coordinates of a bounding box that will
+* be passed on the same call to Voronoi.
+*
+* @param ring an internal or external ring
+*/
+/*private*/
+void
+MaximumInscribedCircle::addRingSites(const LineString* ring)
+{
+    double fromX, fromY, toX, toY, segmentX, segmentY;
+    Point* fromPoint;
+    Point* toPoint;
+
+    for(unsigned int i = 0; i < ring->getNumPoints(); i++) {
+        fromPoint = ring->getPointN(i);
+        toPoint = ring->getPointN(i+1);
+        fromX = fromPoint->getX();
+        fromY = fromPoint->getY();
+        toX = toPoint->getX();
+        toY = toPoint->getY();
+
+        // Push fromPoint
+        sites.push_back(*(fromPoint->getCoordinate()));
+
+        // Push segments
+        for(unsigned int k = 0; k < *inputNumSegments; k++) {
+            segmentX = fromX + (toX - fromX) * k / *inputNumSegments;
+            segmentY = fromY + (toY - fromY) * k / *inputNumSegments;
+            sites.push_back(Coordinate(segmentX, segmentY));
+        }
+
+        // Update bounding box
+        if(fromX < xmin) {
+            xmin = fromX;
+        }
+        if(fromX > xmax) {
+            xmax = fromX;
+        }
+        if(fromY < ymin) {
+            ymin = fromY;
+        }
+        if(fromY > ymax) {
+            ymax = fromY;
+        }
+    }
+}
+
+// TODO: Figure out if I can use one of the extractor functions defined
+//       in GEOSGeom or MakeValid.
+static std::unique_ptr<geom::Geometry> extractUniquePoints(const geom::Geometry* geom)
+{
+
+    // Code taken from GEOSGeom_extractUniquePoints_r()
+
+    /* 1: extract points */
+    std::vector<const geom::Coordinate*> coords;
+    UniqueCoordinateArrayFilter filter(coords);
+    geom->apply_ro(&filter);
+
+    /* 2: for each point, create a geometry and put into a vector */
+    std::vector<std::unique_ptr<Geometry>> points;
+    points.reserve(coords.size());
+    const GeometryFactory* factory = geom->getFactory();
+    for(const Coordinate* c : coords) {
+        points.emplace_back(factory->createPoint(*c));
+    }
+
+    /* 3: create a multipoint */
+    return factory->createMultiPoint(std::move(points));
+}
+
+/*private*/
+void
+MaximumInscribedCircle::computeVoronoiVertices()
+{
+    VoronoiDiagramBuilder builder;
+    CoordinateSequence* coords;
+    CoordinateSequenceFactory* coordSeqFactory;
+    const GeometryFactory& geomFact(*GeometryFactory::getDefaultInstance());
+    std::unique_ptr<Geometry> results;
+    builder.setSites(*(coordSeqFactory->create(&sites)));
+
+    results = builder.getDiagramEdges(geomFact);
+    for(unsigned int i = 0; i < results->getNumGeometries(); i++) {
+        coords = extractUniquePoints(results->getGeometryN(i))->getCoordinates();
+        for(unsigned int j = 0; j < coords->getSize(); j++) {
+            voronoiVertices.push_back(coords->getAt(j));
+        }
+    }
+}
+
+} // namespace geos.algorithm
+} // namespace geos
