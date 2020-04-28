3d59b7babeacf5c0fd5d09b3fe914b9c7a558564
Paul Ramsey <pramsey@cleverelephant.ca>
2020-04-28 12:15:28 -0700

    quiet msvc warnings

--- tests/unit/operation/distance/IndexedFacetDistanceTest.cpp
+++ tests/unit/operation/distance/IndexedFacetDistanceTest.cpp
@@ -27,6 +27,10 @@
 #include <geos/operation/distance/DistanceOp.h>
 #include <geos/operation/distance/IndexedFacetDistance.h>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 namespace tut {
 //
 // Test Group
