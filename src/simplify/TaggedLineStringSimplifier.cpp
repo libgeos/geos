c50c79373e7362c8c971787394bd985dfa247d12
Even Rouault <even.rouault@spatialys.com>
2020-06-27 21:46:25 +0200

    TaggedLineStringSimplifier.cpp: avoid importing whole std namespace to avoid cppcheck confusing the class' remove method with std::remove

--- src/simplify/TaggedLineStringSimplifier.cpp
+++ src/simplify/TaggedLineStringSimplifier.cpp
@@ -40,7 +40,9 @@
 #endif
 
 using namespace geos::geom;
-using namespace std;
+using std::pair;
+using std::unique_ptr;
+using std::vector;
 
 namespace geos {
 namespace simplify { // geos::simplify
