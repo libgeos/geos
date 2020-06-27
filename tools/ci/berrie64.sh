0f938979b58f9a451670032a7199ece23d52c3c1
Regina Obe <lr@pcorp.us>
2020-06-27 14:07:36 -0400

    Add berrie64 to jenkins bot chain. References #1033

---
+++ tools/ci/berrie64.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+# Raspberry Pi (berrie64) CI script runner for GEOS
+# 64-bit ARM chip
+#
+# Copyright (c) 2020 Regina Obe <lr@pcorp.us>
+#
+# This is free software; you can redistribute and/or modify it under
+# the terms of the GNU Lesser General Public Licence as published
+# by the Free Software Foundation.
+# See the COPYING file for more information.
+#
+# auto tools
+if true; then
+    sh autogen.sh
+    ./configure
+    make
+    make check
+fi
+
+
+# cmake
+if false; then
+    rm -rf build
+    mkdir -p build
+    cd build
+    cmake ../
+    make
+    [ -f CMakeCache.txt ] && \
+        ctest --output-on-failure . || \
+        make check
+fi
