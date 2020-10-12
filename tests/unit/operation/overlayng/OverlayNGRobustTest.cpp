3507276eee3ae0623d9aeb4cb91254844834109c
Sandro Santilli <strk@kbt.io>
2020-10-12 16:27:27 +0200

    Another test for 1051, at another level
    References #1051

--- tests/unit/operation/overlayng/OverlayNGRobustTest.cpp
+++ tests/unit/operation/overlayng/OverlayNGRobustTest.cpp
@@ -44,6 +44,15 @@ struct test_overlayngrobust_data {
         ensure_equals_geometry(geom_expected.get(), geom_result.get());
     }
 
+    void
+    checkOverlaySuccess(const std::string& a, const std::string& b, int opCode)
+    {
+        std::unique_ptr<Geometry> geom_a = r.read(a);
+        std::unique_ptr<Geometry> geom_b = r.read(b);
+        std::unique_ptr<Geometry> geom_expected = r.read(expected);
+		ensure_nothrow( OverlayNGRobust::Overlay(geom_a.get(), geom_b.get(), opCode) );
+    }
+
 };
 
 typedef test_group<test_overlayngrobust_data> group;
@@ -64,6 +73,22 @@ void object::test<1> ()
     std::string b = "MULTIPOLYGON (((832864.275023695 0.0,833978.556808034 -0.000110682755987,833978.556808034 0.0,833978.556808034 0.000110682755987,832864.275023695 0.0,832864.275023695 0.0)),((835092.849076364 0.0,833978.557030887 -0.000110682755987,833978.557030887 0.0,833978.557030887 0.000110682755987,835092.849076364 0.0,835092.849076364 0.0)))";
     std::string exp = "MULTILINESTRING ((832864.275023695 0.0,833978.556808034 0.0),(833978.557030887 0.0,835092.849076364 0.0))";
     testOverlay(a, b, exp, OverlayNG::INTERSECTION);
+}
+
+/**
+ * Tests correct ordering of {@link SegmentNode#compareTo(Object)}.
+ *
+ * See https://trac.osgeo.org/geos/ticket/1051
+ *
+ * JTS equivalent: testSegmentNodeOrderingIntersection()
+ */
+template<>
+template<>
+void object::test<2> ()
+{
+    std::string a = "POLYGON ((654948.3853299792 1794977.105854025, 655016.3812220972 1794939.918901604, 655016.2022581929 1794940.1099794197, 655014.9264068712 1794941.4254068714, 655014.7408834674 1794941.6101225375, 654948.3853299792 1794977.105854025))";
+	std::string b = "POLYGON ((655103.6628454948 1794805.456674405, 655016.20226 1794940.10998, 655014.8317182435 1794941.5196832407, 655014.8295602322 1794941.5218318563, 655014.740883467 1794941.610122538, 655016.6029214273 1794938.7590508445, 655103.6628454948 1794805.456674405))";
+    checkOverlaySuccess(a, b, OverlayNG::INTERSECTION);
 }
 
 
