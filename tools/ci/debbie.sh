a2a41b8c765f789ab929d3ccebf12d791c580bba
Regina Obe <lr@pcorp.us>
2021-02-01 19:02:59 -0500

    Add script for debbie instead of pulling from build-bot repo

---
+++ tools/ci/debbie.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Debian sid CI script runner for GEOS
+# 64-bit Debian Sid (debbie)
+#
+# Copyright (c) 2021 Regina Obe <lr@pcorp.us>
+#
+# This is free software; you can redistribute and/or modify it under
+# the terms of the GNU Lesser General Public Licence as published
+# by the Free Software Foundation.
+# See the COPYING file for more information.
+
+rm -rf build
+mkdir -p build
+cd build
+cmake ../
+make
+[ -f CMakeCache.txt ] && \
+ctest --output-on-failure . || \
+make check
