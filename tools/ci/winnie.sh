3973bc7ed00c9ad192de4f1fbf2bb3ee8903df7b
Regina Obe <lr@pcorp.us>
2021-02-04 12:55:27 -0500

    Forgot to include cmake_path

--- tools/ci/winnie.sh
+++ tools/ci/winnie.sh
@@ -9,12 +9,37 @@
 # the terms of the GNU Lesser General Public Licence as published
 # by the Free Software Foundation.
 # See the COPYING file for more information.
+if [[ "$GCC_TYPE" == "gcc45" ]] ; then #gcc 45 has no extension
+  export GCC_TYPE=
+fi;
+## end variables passed in by jenkins
 
-rm -rf build
-mkdir -p build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=../rel-${GEOS_VER}w${OS_BUILD} ../
+if [ "$OS_BUILD" == "64" ] ; then
+	export MINGHOST=x86_64-w64-mingw32
+else
+	export MINGHOST=i686-w64-mingw32
+fi;
+CMAKE_PATH=/cmake
+
+export PATH="${PATH}:${CMAKE_PATH}/bin:/bin:/include"
+
+cd ../
+rm -rf build${OS_BUILD}${MAKE_TYPE}${GEOS_VER}
+mkdir -p build${OS_BUILD}${MAKE_TYPE}${GEOS_VER}
+cd build${OS_BUILD}${MAKE_TYPE}${GEOS_VER}
+export GEOS_ENABLE_INLINE=ON #for older geos
+if [[ "${DISABLE_GEOS_INLINE}" == "" ]] ; then
+  export DISABLE_GEOS_INLINE=ON
+fi
+echo "Disable geos inline ${DISABLE_GEOS_INLINE}"
+
+if [[ "${OS_BUILD}" == "64" ]] ; then
+  #cmake -G "MSYS Makefiles" DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=${PROJECTS}/geos/rel-${GEOS_VER}w${OS_BUILD}${GCC_TYPE} -DHAVE_STD_ISNAN=1 -DHAVE_LONG_LONG_INT_64=1 -DGEOS_ENABLE_INLINE=NO -DDISABLE_GEOS_INLINE=ON   -  ../${CMAKE_GEOS_VER}
+  cmake -G "MSYS Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=${PROJECTS}/geos/rel-${GEOS_VER}w${OS_BUILD}${GCC_TYPE} -DHAVE_STD_ISNAN=1 -DHAVE_LONG_LONG_INT_64=1 -DGEOS_ENABLE_INLINE=${GEOS_ENABLE_INLINE} -DDISABLE_GEOS_INLINE=${DISABLE_GEOS_INLINE}  -  ../${CMAKE_GEOS_VER}
+else
+  #cmake -G "MSYS Makefiles" DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=${PROJECTS}/geos/rel-${GEOS_VER}w${OS_BUILD}${GCC_TYPE} -DHAVE_STD_ISNAN=1 -DGEOS_ENABLE_INLINE=NO -DDISABLE_GEOS_INLINE=ON   -  ../${CMAKE_GEOS_VER}
+  cmake -G "MSYS Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=${PROJECTS}/geos/rel-${GEOS_VER}w${OS_BUILD}${GCC_TYPE} -DHAVE_STD_ISNAN=1 -DGEOS_ENABLE_INLINE=${GEOS_ENABLE_INLINE} -DDISABLE_GEOS_INLINE=${DISABLE_GEOS_INLINE}  -  ../${CMAKE_GEOS_VER}
+fi
 make && make install
-[ -f CMakeCache.txt ] && \
-ctest --output-on-failure . || \
-make check
+#make check
+ctest --output-on-failure
