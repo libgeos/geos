b26dace7c518a7dd8585a0e0e50dc1f7a189bea9
Paul Ramsey <pramsey@cleverelephant.ca>
2021-04-16 17:19:02 -0700

    Change RepeatedPointFilter to use CoordinateFilter per @dbaston

--- src/operation/valid/RepeatedPointRemover.cpp
+++ src/operation/valid/RepeatedPointRemover.cpp
@@ -15,69 +15,101 @@
 #include <geos/operation/valid/RepeatedPointRemover.h>
 
 #include <geos/geom/Coordinate.h>
-#include <geos/geom/CoordinateArraySequenceFactory.h>
+#include <geos/geom/CoordinateFilter.h>
+
 #include <geos/util.h>
+
+using geos::geom::Coordinate;
+using geos::geom::CoordinateFilter;
+using geos::geom::CoordinateArraySequence;
 
 namespace geos {
 namespace operation {
 namespace valid {
 
-std::unique_ptr<geom::CoordinateArraySequence>
+class RepeatedPointFilter : public CoordinateFilter {
+    public:
+
+        RepeatedPointFilter() : m_prev(nullptr) {}
+
+        void filter_ro(const Coordinate* curr) override final {
+
+            // skip duplicate point
+            if (m_prev != nullptr && curr->equals2D(*m_prev)) {
+                return;
+            }
+
+            m_coords.push_back(*curr);
+            m_prev = curr;
+        }
+
+        std::vector<Coordinate> getCoords() {
+            return std::move(m_coords);
+        }
+
+    private:
+
+        std::vector<Coordinate> m_coords;
+        const Coordinate* m_prev;
+};
+
+std::unique_ptr<CoordinateArraySequence>
 RepeatedPointRemover::removeRepeatedPoints(const geom::CoordinateSequence* seq) {
-    using geom::Coordinate;
 
     if (seq->isEmpty()) {
-        return detail::make_unique<geom::CoordinateArraySequence>(0u, seq->getDimension());
+        return detail::make_unique<CoordinateArraySequence>(0u, seq->getDimension());
     }
 
-    auto pts = detail::make_unique<std::vector<Coordinate>>();
-    auto sz = seq->getSize();
-    pts->reserve(sz); // assume not many points are repeated
-
-    const Coordinate* prevPt = &(seq->getAt(0));
-    pts->push_back(*prevPt) ;
-
-    for (std::size_t i = 1; i < sz; i++) {
-        const Coordinate* curPt = &(seq->getAt(i));
-        if (*curPt != *prevPt) {
-            pts->push_back(*curPt);
-            prevPt = curPt;
-        }
-    }
-
-    return detail::make_unique<geom::CoordinateArraySequence>(pts.release(), seq->getDimension());
+    RepeatedPointFilter filter;
+    seq->apply_ro(&filter);
+    return detail::make_unique<CoordinateArraySequence>(filter.getCoords());
 }
+
+class RepeatedInvalidPointFilter : public CoordinateFilter {
+    public:
+
+        RepeatedInvalidPointFilter() : m_prev(nullptr) {}
+
+        void filter_ro(const Coordinate* curr) override final {
+
+            bool invalid = ! curr->isValid();
+            // skip initial invalids
+            if (m_prev == nullptr && invalid)
+                return;
+
+            // skip duplicate/invalid points
+            if (m_prev != nullptr &&
+                (invalid || curr->equals2D(*m_prev)) ) {
+                return;
+            }
+
+            m_coords.push_back(*curr);
+            m_prev = curr;
+        }
+
+        std::vector<Coordinate> getCoords() {
+            return std::move(m_coords);
+        }
+
+    private:
+
+        std::vector<Coordinate> m_coords;
+        const Coordinate* m_prev;
+};
+
+
+
 
 std::unique_ptr<geom::CoordinateArraySequence>
 RepeatedPointRemover::removeRepeatedAndInvalidPoints(const geom::CoordinateSequence* seq) {
-    using geom::Coordinate;
-    std::size_t start = 0;
 
     if (seq->isEmpty()) {
-        return detail::make_unique<geom::CoordinateArraySequence>(0u, seq->getDimension());
+        return detail::make_unique<CoordinateArraySequence>(0u, seq->getDimension());
     }
 
-    auto pts = detail::make_unique<std::vector<Coordinate>>();
-    auto sz = seq->getSize();
-    pts->reserve(sz); // assume not many points are repeated
-
-    // Find first valid poit
-    while(!seq->getAt(start).isValid()) {
-        start++;
-    }
-
-    const Coordinate* prevPt = &(seq->getAt(start));
-    pts->push_back(*prevPt);
-
-    for (std::size_t i = start+1; i < sz; i++) {
-        const Coordinate* curPt = &(seq->getAt(i));
-        if (*curPt != *prevPt && curPt->isValid()) {
-            pts->push_back(*curPt);
-            prevPt = curPt;
-        }
-    }
-
-    return detail::make_unique<geom::CoordinateArraySequence>(pts.release(), seq->getDimension());
+    RepeatedInvalidPointFilter filter;
+    seq->apply_ro(&filter);
+    return detail::make_unique<CoordinateArraySequence>(filter.getCoords());
 }
 
 
