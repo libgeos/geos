74fbee9cb0dd41acf34e0c2c8eba09a53566a370
caspervdw <caspervdw@gmail.com>
2021-05-27 21:56:07 +0200

    Fix tests

--- tests/unit/capi/GEOSGeomReadGeoJSONTest.cpp
+++ tests/unit/capi/GEOSGeomReadGeoJSONTest.cpp
@@ -14,28 +14,24 @@ namespace tut {
 // Common data used in test cases.
 struct test_capigeosgeomreadgeojson_data : public capitest::utility {
     GEOSGeoJSONReader* reader_;
-    GEOSWKTReader* wkt_reader_;
 
-    test_capigeosgeomreadgeojson_data() : reader_(nullptr), wkt_reader_(nullptr)
+    test_capigeosgeomreadgeojson_data() : reader_(nullptr)
     {
         reader_ = GEOSGeoJSONReader_create();
-        wkt_reader_ = GEOSWKTReader_create();
     }
 
     ~test_capigeosgeomreadgeojson_data()
     {
         GEOSGeoJSONReader_destroy(reader_);
-        GEOSWKTReader_destroy(wkt_reader_);
         reader_ = nullptr;
-        wkt_reader_ = nullptr;
     }
 
     void
     test_geojson(std::string const& geojson, std::string const& wkt)
     {
         geom1_ = GEOSGeoJSONReader_read(reader_, &geojson[0]);
-        expected_ = GEOSWKTReader_read(wkt_reader_, &wkt[0]);
-        ensure_geometry_equals(geom1_, expected_);
+        ensure("GEOSGeoJSONReader_read failed to create geometry", nullptr != geom1_);
+        ensure_geometry_equals(geom1_, fromWKT(&wkt[0]));
     }
 };
 
@@ -53,9 +49,28 @@ template<>
 void object::test<1>
 ()
 {
-    // POINT(1.234 5.678)
-    std::string geojson(R"({"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Point","coordinates":[1.234,5.678]}}])");
-    std::string wkt("GEOMETRYCOLLECTION(POINT(1.234 5.678))");
+    std::string geojson("{\"type\":\"Point\",\"coordinates\":[-117.0,33.0]}");
+    std::string wkt("POINT(-117.0 33.0)");
     test_geojson(geojson, wkt);
+}
+
+template<>
+template<>
+void object::test<2>
+()
+{
+    std::string geojson("{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"geometry\":{\"type\":\"Point\",\"coordinates\":[-117.0,33.0]}},{\"type\":\"Feature\",\"geometry\":{\"type\":\"Point\",\"coordinates\":[-122.0,45.0]}}]}");
+    std::string wkt("GEOMETRYCOLLECTION (POINT (-117.000 33.000), POINT (-122.000 45.000))");
+    test_geojson(geojson, wkt);
+}
+
+template<>
+template<>
+void object::test<3>
+()
+{
+    std::string geojson("<gml>NOT_GEO_JSON</gml>");
+    geom1_ = GEOSGeoJSONReader_read(reader_, &geojson[0]);
+    ensure(geom1_ == nullptr);
 }
 }
