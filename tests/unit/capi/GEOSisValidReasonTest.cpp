39ab31cba740285700b13357984a31130d18ed85
Paul Ramsey <pramsey@cleverelephant.ca>
2021-10-04 09:13:24 -0700

    Content of IsValidTest and IsValidReasonTest were reversed for some reason.

--- tests/unit/capi/GEOSisValidReasonTest.cpp
+++ tests/unit/capi/GEOSisValidReasonTest.cpp
@@ -9,12 +9,12 @@ namespace tut {
 // Test Group
 //
 
-struct test_geosisvalid_data : public capitest::utility {};
+struct test_geosisvalidreason_data : public capitest::utility {};
 
-typedef test_group<test_geosisvalid_data> group;
+typedef test_group<test_geosisvalidreason_data> group;
 typedef group::object object;
 
-group test_geosisvalid("capi::GEOSisValid");
+group test_geosisvalidreason("capi::GEOSisValidReason");
 
 template<>
 template<>
@@ -22,9 +22,12 @@ void object::test<1>()
 {
     GEOSGeometry* input = GEOSGeomFromWKT("LINESTRING (1 2, 4 5, 9 -2)");
 
-    ensure_equals(1, GEOSisValid(input));
+    char* reason = GEOSisValidReason(input);
+
+    ensure_equals(std::string(reason), "Valid Geometry");
 
     GEOSGeom_destroy(input);
+    GEOSFree(reason);
 }
 
 template<>
@@ -33,9 +36,12 @@ void object::test<2>()
 {
     GEOSGeometry* input = GEOSGeomFromWKT("POLYGON ((0 0, 1 0, 0 1, 1 1, 0 0))");
 
-    ensure_equals(0, GEOSisValid(input));
+    char* reason = GEOSisValidReason(input);
+
+    ensure_equals(std::string(reason), "Self-intersection[0.5 0.5]");
 
     GEOSGeom_destroy(input);
+    GEOSFree(reason);
 }
 
 
--- tests/unit/capi/GEOSisValidTest.cpp
+++ tests/unit/capi/GEOSisValidTest.cpp
@@ -9,12 +9,12 @@ namespace tut {
 // Test Group
 //
 
-struct test_geosisvalidreason_data : public capitest::utility {};
+struct test_geosisvalid_data : public capitest::utility {};
 
-typedef test_group<test_geosisvalidreason_data> group;
+typedef test_group<test_geosisvalid_data> group;
 typedef group::object object;
 
-group test_geosisvalidreason("capi::GEOSisValidReason");
+group test_geosisvalid("capi::GEOSisValid");
 
 template<>
 template<>
@@ -22,12 +22,9 @@ void object::test<1>()
 {
     GEOSGeometry* input = GEOSGeomFromWKT("LINESTRING (1 2, 4 5, 9 -2)");
 
-    char* reason = GEOSisValidReason(input);
-
-    ensure_equals(std::string(reason), "Valid Geometry");
+    ensure_equals(1, GEOSisValid(input));
 
     GEOSGeom_destroy(input);
-    GEOSFree(reason);
 }
 
 template<>
@@ -36,14 +33,30 @@ void object::test<2>()
 {
     GEOSGeometry* input = GEOSGeomFromWKT("POLYGON ((0 0, 1 0, 0 1, 1 1, 0 0))");
 
-    char* reason = GEOSisValidReason(input);
-
-    ensure_equals(std::string(reason), "Self-intersection[0.5 0.5]");
+    ensure_equals(0, GEOSisValid(input));
 
     GEOSGeom_destroy(input);
-    GEOSFree(reason);
 }
 
 
-} // namespace tut
+// Unclosed polygon
+template<>
+template<>
+void object::test<3>()
+{
+    GEOSCoordSequence* shell_seq = GEOSCoordSeq_create(4, 2);
+    double shell_coords[] = {0,0, 0,10, 10,10, 10,0};
+    for (unsigned int i = 0; i < 4; i++) {
+        GEOSCoordSeq_setXY(shell_seq, i, shell_coords[2*i], shell_coords[2*i+1]);
+    }
 
+    GEOSGeometry* shell = GEOSGeom_createLinearRing(shell_seq);
+    ensure(shell != nullptr);
+    GEOSGeometry* polygon = GEOSGeom_createPolygon(shell, nullptr, 0);
+    ensure(polygon != nullptr);
+    char isvalid = GEOSisValid(polygon);
+    ensure_equals(0, isvalid);
+    GEOSGeom_destroy(polygon);
+}
+
+} // namespace tut
