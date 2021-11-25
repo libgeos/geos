a0c9de47005a669fe6f8dc668bc5faf78c57d0b2
Regina Obe <lr@pcorp.us>
2021-11-24 23:06:22 -0500

    Change berries to also install GEOS in custom location for PostGIS testing.

--- tools/ci/berrie64.sh
+++ tools/ci/berrie64.sh
@@ -3,18 +3,22 @@
 # Raspberry Pi (berrie64) CI script runner for GEOS
 # 64-bit ARM chip
 #
-# Copyright (c) 2020 Regina Obe <lr@pcorp.us>
+# Copyright (c) 2020-2021 Regina Obe <lr@pcorp.us>
 #
 # This is free software; you can redistribute and/or modify it under
 # the terms of the GNU Lesser General Public Licence as published
 # by the Free Software Foundation.
 # See the COPYING file for more information.
 
+# read version from Version.txt file
+. Version.txt
+echo ${JENKINS_HOME}
+export REL_PATH=~/workspace/geos/rel-${GEOS_VERSION_MAJOR}.${GEOS_VERSION_MINOR}.${GEOS_VERSION_PATCH}
 rm -rf build
 mkdir -p build
 cd build
-cmake ../
-make
+cmake -DCMAKE_INSTALL_PREFIX:PATH=${REL_PATH} ../
+make && make install
 [ -f CMakeCache.txt ] && \
 ctest --output-on-failure . || \
 make check
