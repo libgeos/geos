0ce37afb99c1dd31a6960fa42d6d56647e85e1c8
Sandro Santilli <strk@kbt.io>
2022-01-12 14:00:52 +0100

    Fix MonotoneChainBuilder::getChain with empty CoordinateString
    See https://github.com/libgeos/geos/issues/539
    
    Includes unit test and NEWS item
    
    Also moves Geometry::difference test where it belongs, and enable
    it (it was previously disabled).

--- tests/unit/index/chain/MonotoneChainBuilderTest.cpp
+++ tests/unit/index/chain/MonotoneChainBuilderTest.cpp
@@ -4,14 +4,12 @@
 #include <tut/tut.hpp>
 #include <utility.h>
 // geos
-#include <geos/io/WKTReader.h>
-#include <geos/io/WKTWriter.h>
-#include <geos/geom/PrecisionModel.h>
-#include <geos/geom/GeometryFactory.h>
-#include <geos/geom/Geometry.h>
-#include <geos/geom/Point.h>
-#include <geos/geom/LineString.h>
-#include <geos/util.h>
+#include <geos/geom/Coordinate.h>
+#include <geos/geom/CoordinateSequence.h>
+#include <geos/geom/CoordinateArraySequence.h>
+#include <geos/index/chain/MonotoneChain.h>
+#include <geos/index/chain/MonotoneChainBuilder.h>
+
 
 namespace tut {
 // dummy data, not used
@@ -33,28 +31,19 @@ group test_monotonechain_group("geos::index::chain::MonotoneChainBuilder");
 //
 // Test Cases
 
-
 // This issue exercised a bug in MonotoneChainBuilder
-// https://github.com/libgeos/geos/issues/290
+// https://github.com/libgeos/geos/issues/539
 template<>
 template<>
 void object::test<1>
 ()
 {
-    std::string wkt1("POLYGON((0 0, 0 2, 2 2, 2 0, 0 0))");
-    std::unique_ptr<geos::geom::Geometry> g1(wktreader.read(wkt1));
+    std::vector< std::unique_ptr<geos::index::chain::MonotoneChain> > chain;
+    geos::geom::CoordinateArraySequence pts;
 
-    std::string wkt2("POLYGON((0.1 0.1, 4.0 0.1, 4.0 1.9, 0.1 1.9, 0.1 0.1))");
-    std::unique_ptr<geos::geom::Geometry> g2(wktreader.read(wkt2));
+    geos::index::chain::MonotoneChainBuilder::getChains(&pts, 0, chain);
 
-    auto result = g2->difference(g1.get());
-    result->normalize();
-
-    std::string wktExpected("POLYGON ((2 0.1, 2 1.9, 4 1.9, 4 0.1, 2 0.1))");
-    std::unique_ptr<geos::geom::Geometry> expected(wktreader.read(wktExpected));
-
-    ensure_equals_geometry(expected.get(), result.get());
+    ensure_equals(chain.size(), 0u);
 }
-
 
 } // namespace tut
