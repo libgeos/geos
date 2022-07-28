3a8a7ee4f02de109b7bc092fc6c158465cd3c32b
Paul Ramsey <pramsey@cleverelephant.ca>
2022-07-28 10:19:12 -0700

    Add unit tests

--- tests/unit/operation/overlayng/CoverageUnionNGTest.cpp
+++ tests/unit/operation/overlayng/CoverageUnionNGTest.cpp
@@ -27,7 +27,7 @@ struct test_coverageunionng_data {
     WKTWriter w;
 
     void
-    checkCoverageUnion(const std::string& wkt, const std::string& wktExpected)
+    checkUnion(const std::string& wkt, const std::string& wktExpected)
     {
         std::unique_ptr<Geometry> geom = r.read(wkt);
         std::unique_ptr<Geometry> expected = r.read(wktExpected);
@@ -53,7 +53,7 @@ template<>
 template<>
 void object::test<1> ()
 {
-    checkCoverageUnion(
+    checkUnion(
         "MULTIPOLYGON (((100 200, 200 200, 200 100, 100 100, 100 200), (120 180, 180 180, 180 120, 120 120, 120 180)), ((180 120, 120 120, 120 180, 180 180, 180 120)))",
         "POLYGON ((200 200, 200 100, 100 100, 100 200, 200 200))"
         );
@@ -64,10 +64,108 @@ template<>
 template<>
 void object::test<2> ()
 {
-    checkCoverageUnion(
+    checkUnion(
         "MULTIPOLYGON (((1 4, 3 4, 3 2, 1 2, 1 4)), ((5 4, 5 2, 3 2, 3 4, 5 4)), ((7 4, 7 2, 5 2, 5 4, 7 4)))",
         "POLYGON ((3 4, 5 4, 7 4, 7 2, 5 2, 3 2, 1 2, 1 4, 3 4))"
         );
 }
+
+
+// testPolygonsSimple
+template<>
+template<>
+void object::test<3> ()
+{
+    checkUnion(
+        "MULTIPOLYGON (((5 5, 1 5, 5 1, 5 5)), ((5 9, 1 5, 5 5, 5 9)), ((9 5, 5 5, 5 9, 9 5)), ((9 5, 5 1, 5 5, 9 5)))",
+        "POLYGON ((1 5, 5 9, 9 5, 5 1, 1 5))");
+}
+
+// testPolygonsConcentricDonuts
+template<>
+template<>
+void object::test<4> ()
+{
+    checkUnion(
+        "MULTIPOLYGON (((1 9, 9 9, 9 1, 1 1, 1 9), (2 8, 8 8, 8 2, 2 2, 2 8)), ((3 7, 7 7, 7 3, 3 3, 3 7), (4 6, 6 6, 6 4, 4 4, 4 6)))",
+        "MULTIPOLYGON (((9 1, 1 1, 1 9, 9 9, 9 1), (8 8, 2 8, 2 2, 8 2, 8 8)), ((7 7, 7 3, 3 3, 3 7, 7 7), (4 4, 6 4, 6 6, 4 6, 4 4)))");
+}
+
+// testPolygonsConcentricHalfDonuts
+template<>
+template<>
+void object::test<5> ()
+{
+    checkUnion(
+        "MULTIPOLYGON (((6 9, 1 9, 1 1, 6 1, 6 2, 2 2, 2 8, 6 8, 6 9)), ((6 9, 9 9, 9 1, 6 1, 6 2, 8 2, 8 8, 6 8, 6 9)), ((5 7, 3 7, 3 3, 5 3, 5 4, 4 4, 4 6, 5 6, 5 7)), ((5 4, 5 3, 7 3, 7 7, 5 7, 5 6, 6 6, 6 4, 5 4)))",
+        "MULTIPOLYGON (((1 9, 6 9, 9 9, 9 1, 6 1, 1 1, 1 9), (2 8, 2 2, 6 2, 8 2, 8 8, 6 8, 2 8)), ((5 3, 3 3, 3 7, 5 7, 7 7, 7 3, 5 3), (5 4, 6 4, 6 6, 5 6, 4 6, 4 4, 5 4)))");
+}
+
+// testPolygonsNested
+template<>
+template<>
+void object::test<6> ()
+{
+    checkUnion(
+        "GEOMETRYCOLLECTION (POLYGON ((1 9, 9 9, 9 1, 1 1, 1 9), (3 7, 3 3, 7 3, 7 7, 3 7)), POLYGON ((3 7, 7 7, 7 3, 3 3, 3 7)))",
+        "POLYGON ((1 1, 1 9, 9 9, 9 1, 1 1))");
+}
+
+// testPolygonsFormingHole
+template<>
+template<>
+void object::test<7> ()
+{
+    checkUnion(
+        "MULTIPOLYGON (((1 1, 4 3, 5 6, 5 9, 1 1)), ((1 1, 9 1, 6 3, 4 3, 1 1)), ((9 1, 5 9, 5 6, 6 3, 9 1)))",
+        "POLYGON ((9 1, 1 1, 5 9, 9 1), (6 3, 5 6, 4 3, 6 3))");
+}
+
+// testPolygonsSquareGrid
+template<>
+template<>
+void object::test<8> ()
+{
+    checkUnion(
+        "MULTIPOLYGON (((0 0, 0 25, 25 25, 25 0, 0 0)), ((0 25, 0 50, 25 50, 25 25, 0 25)), ((0 50, 0 75, 25 75, 25 50, 0 50)), ((0 75, 0 100, 25 100, 25 75, 0 75)), ((25 0, 25 25, 50 25, 50 0, 25 0)), ((25 25, 25 50, 50 50, 50 25, 25 25)), ((25 50, 25 75, 50 75, 50 50, 25 50)), ((25 75, 25 100, 50 100, 50 75, 25 75)), ((50 0, 50 25, 75 25, 75 0, 50 0)), ((50 25, 50 50, 75 50, 75 25, 50 25)), ((50 50, 50 75, 75 75, 75 50, 50 50)), ((50 75, 50 100, 75 100, 75 75, 50 75)), ((75 0, 75 25, 100 25, 100 0, 75 0)), ((75 25, 75 50, 100 50, 100 25, 75 25)), ((75 50, 75 75, 100 75, 100 50, 75 50)), ((75 75, 75 100, 100 100, 100 75, 75 75)))",
+        "POLYGON ((0 25, 0 50, 0 75, 0 100, 25 100, 50 100, 75 100, 100 100, 100 75, 100 50, 100 25, 100 0, 75 0, 50 0, 25 0, 0 0, 0 25))");
+}
+
+/**
+* Sequential lines are still noded
+*/
+template<>
+template<>
+void object::test<9> ()
+{
+    checkUnion(
+        "MULTILINESTRING ((1 1, 5 1), (9 1, 5 1))",
+        "MULTILINESTRING ((1 1, 5 1), (5 1, 9 1))");
+}
+
+/**
+* Overlapping lines are noded with common portions merged
+*/
+template<>
+template<>
+void object::test<10> ()
+{
+    checkUnion(
+        "MULTILINESTRING ((1 1, 2 1, 3 1), (4 1, 3 1, 2 1))",
+        "MULTILINESTRING ((1 1, 2 1), (2 1, 3 1), (3 1, 4 1))");
+}
+
+/**
+* A network of lines is dissolved noded at degree > 2 vertices
+*/
+template<>
+template<>
+void object::test<11> ()
+{
+    checkUnion(
+        "MULTILINESTRING ((1 9, 3.1 8, 5 7, 7 8, 9 9), (5 7, 5 3, 4 3, 2 3), (9 5, 7 4, 5 3, 8 1))",
+        "MULTILINESTRING ((1 9, 3.1 8), (2 3, 4 3), (3.1 8, 5 7), (4 3, 5 3), (5 3, 5 7), (5 3, 7 4), (5 3, 8 1), (5 7, 7 8), (7 4, 9 5), (7 8, 9 9))");
+}
+
 
 } // namespace tut
