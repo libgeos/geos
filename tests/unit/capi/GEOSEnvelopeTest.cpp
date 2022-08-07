2507402cea8e90937602fe08270c96776f32db74
Daniel Baston <dbaston@gmail.com>
2022-08-07 07:54:26 -0400

    Fix docs, add tests for GEOSEnvelope edge cases

--- tests/unit/capi/GEOSEnvelopeTest.cpp
+++ tests/unit/capi/GEOSEnvelopeTest.cpp
@@ -16,6 +16,7 @@ typedef group::object object;
 
 group test_geosenvelope("capi::GEOSEnvelope");
 
+// non-degenerate input
 template<>
 template<>
 void object::test<1>
@@ -26,6 +27,60 @@ void object::test<1>
     GEOSGeometry* expected = GEOSGeomFromWKT("POLYGON ((1 -2, 9 -2, 9 5, 1 5, 1 -2))");
 
     ensure_equals(GEOSEqualsExact(result, expected, 0), 1);
+
+    GEOSGeom_destroy(input);
+    GEOSGeom_destroy(result);
+    GEOSGeom_destroy(expected);
+}
+
+// point input
+template<>
+template<>
+void object::test<2>
+()
+{
+    GEOSGeometry* input = GEOSGeomFromWKT("POINT (3 8)");
+    GEOSGeometry* result = GEOSEnvelope(input);
+
+    GEOSGeometry* expected = GEOSGeomFromWKT("POINT (3 8)");
+
+    ensure_geometry_equals(result, expected);
+
+    GEOSGeom_destroy(input);
+    GEOSGeom_destroy(result);
+    GEOSGeom_destroy(expected);
+}
+
+// empty point input
+template<>
+template<>
+void object::test<3>
+()
+{
+    GEOSGeometry* input = GEOSGeomFromWKT("POINT EMPTY");
+    GEOSGeometry* result = GEOSEnvelope(input);
+
+    GEOSGeometry* expected = GEOSGeomFromWKT("POINT EMPTY");
+
+    ensure_geometry_equals(result, expected);
+
+    GEOSGeom_destroy(input);
+    GEOSGeom_destroy(result);
+    GEOSGeom_destroy(expected);
+}
+
+// empty polygon input
+template<>
+template<>
+void object::test<4>
+()
+{
+    GEOSGeometry* input = GEOSGeomFromWKT("POLYGON EMPTY");
+    GEOSGeometry* result = GEOSEnvelope(input);
+
+    GEOSGeometry* expected = GEOSGeomFromWKT("POINT EMPTY");
+
+    ensure_geometry_equals(result, expected);
 
     GEOSGeom_destroy(input);
     GEOSGeom_destroy(result);
