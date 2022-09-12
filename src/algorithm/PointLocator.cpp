e94b56fe4febd81f48cc25f5cdbee0a1e4456977
Dan Baston <dbaston@gmail.com>
2022-09-12 10:35:39 -0400

    Fix crash in GEOSIntersects with empty point inputs (#673)
    Fixes https://trac.osgeo.org/geos/ticket/1110

--- src/algorithm/PointLocator.cpp
+++ src/algorithm/PointLocator.cpp
@@ -153,7 +153,7 @@ PointLocator::locate(const Coordinate& p, const Point* pt)
 {
     // no point in doing envelope test, since equality test is just as fast
     const Coordinate* ptCoord = pt->getCoordinate();
-    if(ptCoord->equals2D(p)) {
+    if(ptCoord != nullptr && ptCoord->equals2D(p)) {
         return Location::INTERIOR;
     }
     return Location::EXTERIOR;
