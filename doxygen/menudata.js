ee4cefea890d470fb745f5c784c6a619695d9d11
dbaston <dbaston@gmail.com>
2022-10-17 23:47:06 +0000

    Deploying to gh-pages from @ libgeos/geos@32348a68c5212c89cfefab46891d2b3aada4ab40 🚀

--- doxygen/menudata.js
+++ doxygen/menudata.js
@@ -114,8 +114,6 @@ var menudata={children:[
 {text:"r",url:"functions_vars.html#index_r"},
 {text:"s",url:"functions_vars.html#index_s"},
 {text:"t",url:"functions_vars.html#index_t"},
-{text:"x",url:"functions_vars.html#index_x"},
-{text:"y",url:"functions_vars.html#index_y"},
 {text:"z",url:"functions_vars.html#index_z"}]},
 {text:"Typedefs",url:"functions_type.html"},
 {text:"Enumerations",url:"functions_enum.html"},
