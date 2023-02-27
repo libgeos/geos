66a374c367fcf44dcee59420ad9d01c87e65feec
Paul Ramsey <pramsey@cleverelephant.ca>
2023-02-27 09:24:48 -0800

    remove use of sprintf

--- capi/geos_ts_c.cpp
+++ capi/geos_ts_c.cpp
@@ -2168,7 +2168,7 @@ extern "C" {
     const char* GEOSversion()
     {
         static char version[256];
-        sprintf(version, "%s", GEOS_CAPI_VERSION);
+        snprintf(version, sizeof(version), "%s", GEOS_CAPI_VERSION);
         return version;
     }
 
