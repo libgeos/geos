e32c1fec40fa488b5364f4b0bf7fcfeefc4baaea
Daniel Baston <dbaston@gmail.com>
2023-02-28 20:06:56 -0500

    Doxygen fix?

--- include/geos/index/chain/MonotoneChain.h
+++ include/geos/index/chain/MonotoneChain.h
@@ -95,6 +95,8 @@ public:
     /// @param context
     ///   Ownership left to caller, this class holds a reference.
     ///
+    /// @param expansionDistance distance by which envelope should be expanded
+    ///
     MonotoneChain(const geom::CoordinateSequence& pts,
                   std::size_t start, std::size_t end, void* context, double expansionDistance);
 
