09c10ed41c826ed0ea6c02da6f80709e4f30fe2b
Martin Davis <mtnclimb@gmail.com>
2023-04-24 19:17:30 -0700

    Add note to try sorting in CoverageUnion

--- src/coverage/CoverageUnion.cpp
+++ src/coverage/CoverageUnion.cpp
@@ -18,6 +18,7 @@
 #include <geos/geom/GeometryCollection.h>
 #include <geos/geom/GeometryFactory.h>
 #include <geos/operation/overlayng/CoverageUnion.h>
+//#include <geos/shape/fractal/HilbertEncoder.h>
 
 using geos::geom::Geometry;
 using geos::geom::GeometryCollection;
@@ -35,6 +36,10 @@ CoverageUnion::Union(std::vector<const Geometry*>& coverage)
     if (coverage.size() == 0)
         return nullptr;
 
+    //TODO? spatial sort polgyons to improve performance
+    // Test results are somewhat inconclusive
+    //shape::fractal::HilbertEncoder::sort(coverage.begin(), coverage.end());
+
     const GeometryFactory* geomFact = coverage[0]->getFactory();
     std::unique_ptr<GeometryCollection> geoms(geomFact->createGeometryCollection(coverage));
     return operation::overlayng::CoverageUnion::geomunion(geoms.get());
@@ -43,5 +48,3 @@ CoverageUnion::Union(std::vector<const Geometry*>& coverage)
 
 } // namespace geos.coverage
 } // namespace geos
-
-
