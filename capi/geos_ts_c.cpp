bc2a209e3f05b06325701f68d7a1930c50773bd8
Martin Davis <mtnclimb@gmail.com>
2023-05-08 13:16:41 -0700

    Fix OffsetCurve EndCap parameter handling (#899)

--- capi/geos_ts_c.cpp
+++ capi/geos_ts_c.cpp
@@ -1167,7 +1167,7 @@ extern "C" {
     {
         return execute(extHandle, [&]() {
             BufferParameters bp;
-            bp.setEndCapStyle(BufferParameters::CAP_FLAT);
+            //-- use default cap style ROUND 
             bp.setQuadrantSegments(quadsegs);
 
             if(joinStyle > BufferParameters::JOIN_BEVEL) {
