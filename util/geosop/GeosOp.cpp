ecb8f54c67d3724bdd9c8f7fb9e279cc11aba1e2
Mike Taves <mwtoews@gmail.com>
2023-05-15 22:48:15 +1200

    geosop: change output to show Z

--- util/geosop/GeosOp.cpp
+++ util/geosop/GeosOp.cpp
@@ -21,6 +21,7 @@
 #include <geos/io/WKTReader.h>
 #include <geos/io/WKTWriter.h>
 #include <geos/io/WKBReader.h>
+#include <geos/io/WKBWriter.h>
 
 #include <fstream>
 #include <iostream>
@@ -480,15 +481,22 @@ void GeosOp::outputGeometry(const Geometry * geom) {
     }
 
     if (args.format == GeosOpArgs::fmtWKB ) {
-        std::cout << *(geom) << std::endl;
+        // output as hex-encoded WKB
+        WKBWriter writer;
+        writer.setOutputDimension(3);
+        writer.writeHEX(*geom, std::cout);
+        std::cout << std::endl;
     }
     else {
         // output as text/WKT
         WKTWriter writer;
-        // turn off stoopid fixed precision
-        writer.setTrim(true);
+        writer.setOutputDimension(3);
         if (args.precision >= 0) {
              writer.setRoundingPrecision(args.precision);
+        }
+        else {
+            // turn off stoopid fixed precision
+            writer.setTrim(true);
         }
         std::cout << writer.write(geom) << std::endl;
     }
