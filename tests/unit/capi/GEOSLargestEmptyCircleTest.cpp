871c952ce23bf0256d3ebbf655949298e5bd0f5f
Martin Davis <mtnclimb@gmail.com>
2023-07-17 16:15:44 -0700

    improve unit test utils

--- tests/unit/capi/GEOSLargestEmptyCircleTest.cpp
+++ tests/unit/capi/GEOSLargestEmptyCircleTest.cpp
@@ -48,9 +48,8 @@ void object::test<1>
 {
     input_ = GEOSGeomFromWKT("MULTIPOINT ((100 100), (100 200), (200 200), (200 100))");
     result_ = GEOSLargestEmptyCircle(input_, nullptr, 0.001);
-    ensure(nullptr != result_);
     expected_ = GEOSGeomFromWKT("LINESTRING (150 150, 100 100)");
-    ensure(GEOSEqualsExact(result_, expected_, 0.0001) == 1);
+    ensure_geometry_equals_exact(result_, expected_, 0.0001);
 }
 
 // Line obstacles with square boundary
@@ -62,9 +61,8 @@ void object::test<2>
     input_ = GEOSGeomFromWKT("MULTILINESTRING ((40 90, 90 60), (90 40, 40 10))");
     geom2_ = GEOSGeomFromWKT("POLYGON ((0 100, 100 100, 100 0, 0 0, 0 100))");
     result_ = GEOSLargestEmptyCircle(input_, geom2_, 0.001);
-    ensure(nullptr != result_);
     expected_ = GEOSGeomFromWKT("LINESTRING (0.00038147 49.99961853, 40 10)");
-    ensure(GEOSEqualsExact(result_, expected_, 0.0001) == 1);
+    ensure_geometry_equals_exact(result_, expected_, 0.0001);
 }
 
 } // namespace tut
