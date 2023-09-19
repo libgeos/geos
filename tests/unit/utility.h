e8949f3076577771582286f10e51f4dd210e9853
Martin Davis <mtnclimb@gmail.com>
2023-09-18 18:59:58 -0700

    Fix PreparedLineStringDistance for lines within envelope and polygons (#959)

--- tests/unit/utility.h
+++ tests/unit/utility.h
@@ -97,6 +97,23 @@ instanceOf(InstanceType const* instance)
 }
 
 inline void
+ensure_equals_xy(geos::geom::Coordinate const& actual,
+                  geos::geom::Coordinate const& expected)
+{
+    ensure_equals("Coordinate X", actual.x, expected.x );
+    ensure_equals("Coordinate Y", actual.y, expected.y );
+}
+
+inline void
+ensure_equals_xy(geos::geom::Coordinate const& actual,
+                  geos::geom::Coordinate const& expected,
+                  double tol)
+{
+    ensure_equals("Coordinate X", actual.x, expected.x, tol );
+    ensure_equals("Coordinate Y", actual.y, expected.y, tol );
+}
+
+inline void
 ensure_equals_xyz(geos::geom::Coordinate const& actual,
                   geos::geom::Coordinate const& expected)
 {
