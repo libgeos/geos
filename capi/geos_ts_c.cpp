dbaston/libgeos

Add GEOSGridIntersectionFractions, GEOSSubdivideByGrid

capi/geos_ts_c.cpp
@@ -80,6 +80,8 @@
 #include <geos/operation/cluster/GeometryIntersectsClusterFinder.h>
 #include <geos/operation/distance/DistanceOp.h>
 #include <geos/operation/distance/IndexedFacetDistance.h>
+#include <geos/operation/grid/Grid.h>
+#include <geos/operation/grid/GridIntersection.h>
 #include <geos/operation/linemerge/LineMerger.h>
 #include <geos/operation/intersection/Rectangle.h>
 #include <geos/operation/intersection/RectangleIntersection.h>
@@ -1722,6 +1724,41 @@ extern "C" {
             std::unique_ptr<Geometry> g3 = RectangleIntersection::clip(*g, rect);
             g3->setSRID(g->getSRID());
             return g3.release();
+        });
+    }
+
+    Geometry*
+    GEOSSubdivideByGrid_r(GEOSContextHandle_t extHandle, const Geometry* g, double xmin, double ymin,
+                          double xmax, double ymax, unsigned nx, unsigned ny, int include_exterior)
+    {
+        return execute(extHandle, [&]() {
+            Envelope env(xmin, xmax, ymin, ymax);
+            double dx = env.getWidth() / static_cast<double>(nx);
+            double dy = env.getHeight() / static_cast<double>(ny);
+            geos::operation::grid::Grid<geos::operation::grid::bounded_extent> grid(env, dx, dy);
+
+            return geos::operation::grid::GridIntersection::subdivide_polygon(grid, *g, include_exterior).release();
+        });
+    }
+
+    int
+    GEOSGridIntersectionFractions_r(GEOSContextHandle_t extHandle, const Geometry* g, double xmin, double ymin,
+                                    double xmax, double ymax, unsigned nx, unsigned ny, float* buf)
+    {
+        return execute(extHandle, 0, [&]() {
+            Envelope env(xmin, xmax, ymin, ymax);
+            double dx = env.getWidth() / static_cast<double>(nx);
+            double dy = env.getHeight() / static_cast<double>(ny);
+            geos::operation::grid::Grid<geos::operation::grid::bounded_extent> grid(env, dx, dy);
+
+            // Matrix wants a shared_ptr, but we don't actually want anything to be freed because
+            // buf is externally owned. So we give it an empty deleter.
+            std::shared_ptr<float[]> bufPtr(buf, [](float*) {});
+
+            auto cov = std::make_shared<geos::operation::grid::Matrix<float>>(ny, nx, bufPtr);
+            geos::operation::grid::GridIntersection isect(grid, *g, cov);
+
+            return 1;
         });
     }
 

include/geos/operation/grid/FloodFill.h
@@ -1,0 +1,144 @@
+/**********************************************************************
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.osgeo.org
+ *
+ * Copyright (C) 2018-2025 ISciences, LLC
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation.
+ * See the COPYING file for more information.
+ *
+ **********************************************************************/
+
+#pragma once
+
+#include <geos/operation/grid/Grid.h>
+#include <geos/operation/grid/Matrix.h>
+
+#include <queue>
+
+namespace geos::geom {
+    class Geometry;
+}
+
+namespace geos::algorithm::locate {
+    class PointOnGeometryLocator;
+}
+
+namespace geos::operation::grid {
+
+template<typename T>
+struct fill_values
+{
+    static T EXTERIOR;
+};
+
+template<>
+struct fill_values<float>
+{
+    static constexpr float EXTERIOR{ 0.0f };  // Cell is known to be entirely outside the polygon
+    static constexpr float INTERIOR{ 1.0f };  // Cell is known to be entirely within the polygon
+    static constexpr float FILLABLE{ -1.0f }; // Cell location relative to polygon unknown, but
+                                              // can be determined by fill.
+    static constexpr float UNKNOWN{ -2.0f };  // Cell location relative to polygon unknown
+                                              // and cannot be determined from a flood fill
+                                              // (must be explicitly tested)
+};
+
+class FloodFill
+{
+
+  public:
+    FloodFill(const geom::Geometry& g, const Grid<bounded_extent>& extent);
+
+    ~FloodFill();
+
+    template<typename T>
+    void flood(Matrix<T>& arr) const;
+
+    bool cell_is_inside(size_t i, size_t j) const;
+
+  private:
+    Grid<bounded_extent> m_extent;
+    const geom::Geometry& m_g;
+    std::unique_ptr<algorithm::locate::PointOnGeometryLocator> m_loc;
+};
+
+template<typename T>
+void
+flood_from_pixel(Matrix<T>& arr, size_t i, size_t j, T fill_value)
+{
+    std::queue<std::pair<size_t, size_t>> locations;
+
+    locations.emplace(i, j);
+
+    while (!locations.empty()) {
+        i = locations.front().first;
+        j = locations.front().second;
+        locations.pop();
+
+        if (arr(i, j) == fill_value) {
+            continue;
+        }
+
+        // Left
+        if (j > 0 && arr(i, j - 1) == fill_values<T>::FILLABLE) {
+            locations.emplace(i, j - 1);
+        }
+
+        auto j0 = j;
+
+        // Fill along this row until we hit something
+        for (; j < arr.cols() && arr(i, j) == fill_values<T>::FILLABLE; j++) {
+            arr(i, j) = fill_value;
+        }
+
+        auto j1 = j;
+
+        // Initiate scanlines above our current row
+        if (i > 0) {
+            for (j = j0; j < j1; j++) {
+                // Up
+                if (arr(i - 1, j) == fill_values<T>::FILLABLE) {
+                    locations.emplace(i - 1, j);
+                }
+            }
+        }
+
+        // Initiate scanlines below our current row
+        if (i < arr.rows() - 1) {
+            for (j = j0; j < j1; j++) {
+                // Down
+                if (arr(i + 1, j) == fill_values<T>::FILLABLE) {
+                    locations.emplace(i + 1, j);
+                }
+            }
+        }
+    }
+}
+
+template<typename T>
+void
+FloodFill::flood(Matrix<T>& arr) const
+{
+
+    for (size_t i = 0; i < arr.rows(); i++) {
+        for (size_t j = 0; j < arr.cols(); j++) {
+            if (arr(i, j) == fill_values<T>::UNKNOWN) {
+                throw std::runtime_error("Cell with unknown position encountered.");
+            } else if (arr(i, j) == fill_values<T>::FILLABLE) {
+                // Cell position relative to polygon is unknown but can
+                // be determined from adjacent cells.
+                if (cell_is_inside(i, j)) {
+                    flood_from_pixel(arr, i, j, fill_values<T>::INTERIOR);
+                } else {
+                    flood_from_pixel(arr, i, j, fill_values<T>::EXTERIOR);
+                }
+            }
+        }
+    }
+}
+
+}

src/operation/grid/FloodFill.cpp
@@ -1,0 +1,42 @@
+/**********************************************************************
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.osgeo.org
+ *
+ * Copyright (C) 2018-2025 ISciences, LLC
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation.
+ * See the COPYING file for more information.
+ *
+ **********************************************************************/
+
+#include <geos/operation/grid/FloodFill.h>
+#include <geos/operation/grid/Grid.h>
+#include <geos/algorithm/locate/IndexedPointInAreaLocator.h>
+#include <geos/geom/Location.h>
+
+using geos::geom::Geometry;
+
+namespace geos::operation::grid {
+
+FloodFill::FloodFill(const Geometry& g, const Grid<bounded_extent>& extent)
+  : m_extent{ extent }
+  , m_g{ g }
+  , m_loc{ nullptr }
+{
+  m_loc = std::make_unique<algorithm::locate::IndexedPointInAreaLocator>(m_g);
+}
+
+FloodFill::~FloodFill() = default;
+
+bool
+FloodFill::cell_is_inside(size_t i, size_t j) const
+{
+    geom::CoordinateXY p(  m_extent.x_for_col(j), m_extent.y_for_row(i) );
+
+    return m_loc->locate(&p) == geom::Location::INTERIOR;
+}
+
+}

src/operation/grid/Grid.cpp
@@ -1,0 +1,99 @@
+/**********************************************************************
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.osgeo.org
+ *
+ * Copyright (C) 2018-2025 ISciences, LLC
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation.
+ * See the COPYING file for more information.
+ *
+ **********************************************************************/
+
+#include <geos/operation/grid/Grid.h>
+
+using geos::geom::Envelope;
+
+namespace geos::operation::grid {
+
+template<>
+Envelope
+Grid<bounded_extent>::grid_cell(size_t row, size_t col) const
+{
+    // The ternary clauses below are used to make sure that the cells along
+    // the right and bottom edges of our grid are slightly larger than m_dx,dy
+    // if needed to make sure that we capture our whole extent. This is necessary
+    // because xmin + nx*m_dx may be less than xmax because of floating point
+    // errors.
+    return {
+        xmin() + static_cast<double>(col) * dx(),
+        row == (rows() - 1) ? ymin() : (ymax() - static_cast<double>(row + 1) * dy()),
+        col == (cols() - 1) ? xmax() : (xmin() + static_cast<double>(col + 1) * dx()),
+        ymax() - static_cast<double>(row) * dy()
+    };
+}
+
+template<>
+Envelope
+Grid<infinite_extent>::grid_cell(size_t row, size_t col) const
+{
+    double cell_xmin, cell_xmax, cell_ymin, cell_ymax;
+
+    constexpr double PADDED_CELL_SIZE = 1.0;
+
+    if (col == 0) {
+        cell_xmin = std::min(xmin() - PADDED_CELL_SIZE, m_domain.getMinX());
+    } else if (col == cols() - 1) {
+        cell_xmin = xmax(); // because rightmost col of regular may have different width from others
+    } else {
+        cell_xmin = xmin() + static_cast<double>(col - 1) * dx();
+    }
+
+    switch (cols() - col) {
+        case 1:
+            cell_xmax = std::max(xmax() + PADDED_CELL_SIZE, m_domain.getMaxX());
+            break;
+        case 2:
+            cell_xmax = xmax();
+            break;
+        default:
+            cell_xmax = xmin() + static_cast<double>(col) * dx();
+    }
+
+    if (row == 0) {
+        cell_ymax = std::max(xmax() + PADDED_CELL_SIZE, m_domain.getMaxX());
+    } else if (row == rows() - 1) {
+        cell_ymax = ymin(); // because bottom row of regular may have different height from others
+    } else {
+        cell_ymax = ymax() - static_cast<double>(row - 1) * dy();
+    }
+
+    switch (rows() - row) {
+        case 1:
+            cell_ymin = std::min(xmin() - PADDED_CELL_SIZE, m_domain.getMinY());
+            break;
+        case 2:
+            cell_ymin = ymin();
+            break;
+        default:
+            cell_ymin = ymax() - static_cast<double>(row) * dy();
+    }
+
+    return { cell_xmin, cell_xmax, cell_ymin, cell_ymax };
+}
+
+Grid<infinite_extent>
+make_infinite(const Grid<bounded_extent>& grid, const Envelope& domain)
+{
+    return { grid.extent(), grid.dx(), grid.dy() , domain };
+}
+
+Grid<bounded_extent>
+make_finite(const Grid<infinite_extent>& grid)
+{
+    return { grid.extent(), grid.dx(), grid.dy() };
+}
+
+}

src/operation/grid/Traversal.cpp
@@ -1,0 +1,106 @@
+/**********************************************************************
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.osgeo.org
+ *
+ * Copyright (C) 2018-2025 ISciences, LLC
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation.
+ * See the COPYING file for more information.
+ *
+ **********************************************************************/
+
+#include <cstddef>
+#include <stdexcept>
+
+#include <geos/operation/grid/Traversal.h>
+
+using geos::geom::CoordinateXY;
+
+namespace geos::operation::grid {
+
+void
+Traversal::add(const CoordinateXY& c)
+{
+    m_coords.push_back(c);
+}
+
+bool
+Traversal::empty() const
+{
+    return m_coords.empty();
+}
+
+void
+Traversal::enter(const CoordinateXY& c, Side s)
+{
+    if (!m_coords.empty()) {
+        throw std::runtime_error("Traversal already started");
+    }
+
+    add(c);
+    m_entry = s;
+}
+
+void
+Traversal::exit(const CoordinateXY& c, Side s)
+{
+    add(c);
+    m_exit = s;
+}
+
+bool
+Traversal::is_closed_ring() const
+{
+    return m_coords.size() >= 3 && m_coords[0] == m_coords[m_coords.size() - 1];
+}
+
+bool
+Traversal::entered() const
+{
+    return m_entry != Side::NONE;
+}
+
+bool
+Traversal::exited() const
+{
+    return m_exit != Side::NONE;
+}
+
+bool
+Traversal::multiple_unique_coordinates() const
+{
+    for (size_t i = 1; i < m_coords.size(); i++) {
+        if (m_coords[0] != m_coords[i]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool
+Traversal::traversed() const
+{
+    return entered() && exited();
+}
+
+const CoordinateXY&
+Traversal::last_coordinate() const
+{
+    return m_coords.at(m_coords.size() - 1);
+}
+
+const CoordinateXY&
+Traversal::exit_coordinate() const
+{
+    if (!exited()) {
+        throw std::runtime_error("Can't get exit coordinate from incomplete traversal.");
+    }
+
+    return last_coordinate();
+}
+
+}

libgeos/geos

capi/geos_ts_c.cpp
@@ -81,6 +81,8 @@
 #include <geos/operation/cluster/GeometryIntersectsClusterFinder.h>
 #include <geos/operation/distance/DistanceOp.h>
 #include <geos/operation/distance/IndexedFacetDistance.h>
+#include <geos/operation/grid/Grid.h>
+#include <geos/operation/grid/GridIntersection.h>
 #include <geos/operation/linemerge/LineMerger.h>
 #include <geos/operation/intersection/Rectangle.h>
 #include <geos/operation/intersection/RectangleIntersection.h>
@@ -1790,6 +1792,41 @@ extern "C" {
             std::unique_ptr<Geometry> g3 = RectangleIntersection::clip(*g, rect);
             g3->setSRID(g->getSRID());
             return g3.release();
+        });
+    }
+
+    Geometry*
+    GEOSSubdivideByGrid_r(GEOSContextHandle_t extHandle, const Geometry* g, double xmin, double ymin,
+                          double xmax, double ymax, unsigned nx, unsigned ny, int include_exterior)
+    {
+        return execute(extHandle, [&]() {
+            Envelope env(xmin, xmax, ymin, ymax);
+            double dx = env.getWidth() / static_cast<double>(nx);
+            double dy = env.getHeight() / static_cast<double>(ny);
+            geos::operation::grid::Grid<geos::operation::grid::bounded_extent> grid(env, dx, dy);
+
+            return geos::operation::grid::GridIntersection::subdividePolygon(grid, *g, include_exterior).release();
+        });
+    }
+
+    int
+    GEOSGridIntersectionFractions_r(GEOSContextHandle_t extHandle, const Geometry* g, double xmin, double ymin,
+                                    double xmax, double ymax, unsigned nx, unsigned ny, float* buf)
+    {
+        return execute(extHandle, 0, [&]() {
+            Envelope env(xmin, xmax, ymin, ymax);
+            double dx = env.getWidth() / static_cast<double>(nx);
+            double dy = env.getHeight() / static_cast<double>(ny);
+            geos::operation::grid::Grid<geos::operation::grid::bounded_extent> grid(env, dx, dy);
+
+            // Matrix wants a shared_ptr, but we don't actually want anything to be freed because
+            // buf is externally owned. So we give it an empty deleter.
+            std::shared_ptr<float[]> bufPtr(buf, [](float*) {});
+
+            auto cov = std::make_shared<geos::operation::grid::Matrix<float>>(ny, nx, bufPtr);
+            geos::operation::grid::GridIntersection isect(grid, *g, cov);
+
+            return 1;
         });
     }
 

include/geos/operation/grid/FloodFill.h
@@ -1,0 +1,144 @@
+/**********************************************************************
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.osgeo.org
+ *
+ * Copyright (C) 2018-2025 ISciences, LLC
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation.
+ * See the COPYING file for more information.
+ *
+ **********************************************************************/
+
+#pragma once
+
+#include <geos/operation/grid/Grid.h>
+#include <geos/operation/grid/Matrix.h>
+
+#include <queue>
+
+namespace geos::geom {
+    class Geometry;
+}
+
+namespace geos::algorithm::locate {
+    class PointOnGeometryLocator;
+}
+
+namespace geos::operation::grid {
+
+template<typename T>
+struct fill_values
+{
+    static T EXTERIOR;
+};
+
+template<>
+struct fill_values<float>
+{
+    static constexpr float EXTERIOR{ 0.0f };  // Cell is known to be entirely outside the polygon
+    static constexpr float INTERIOR{ 1.0f };  // Cell is known to be entirely within the polygon
+    static constexpr float FILLABLE{ -1.0f }; // Cell location relative to polygon unknown, but
+                                              // can be determined by fill.
+    static constexpr float UNKNOWN{ -2.0f };  // Cell location relative to polygon unknown
+                                              // and cannot be determined from a flood fill
+                                              // (must be explicitly tested)
+};
+
+class FloodFill
+{
+
+  public:
+    FloodFill(const geom::Geometry& g, const Grid<bounded_extent>& extent);
+
+    ~FloodFill();
+
+    template<typename T>
+    void flood(Matrix<T>& arr) const;
+
+    bool cellIsInside(size_t i, size_t j) const;
+
+  private:
+    Grid<bounded_extent> m_extent;
+    const geom::Geometry& m_g;
+    std::unique_ptr<algorithm::locate::PointOnGeometryLocator> m_loc;
+};
+
+template<typename T>
+void
+floodFromPixel(Matrix<T>& arr, size_t i, size_t j, T fill_value)
+{
+    std::queue<std::pair<size_t, size_t>> locations;
+
+    locations.emplace(i, j);
+
+    while (!locations.empty()) {
+        i = locations.front().first;
+        j = locations.front().second;
+        locations.pop();
+
+        if (arr(i, j) == fill_value) {
+            continue;
+        }
+
+        // Left
+        if (j > 0 && arr(i, j - 1) == fill_values<T>::FILLABLE) {
+            locations.emplace(i, j - 1);
+        }
+
+        auto j0 = j;
+
+        // Fill along this row until we hit something
+        for (; j < arr.getNumCols() && arr(i, j) == fill_values<T>::FILLABLE; j++) {
+            arr(i, j) = fill_value;
+        }
+
+        auto j1 = j;
+
+        // Initiate scanlines above our current row
+        if (i > 0) {
+            for (j = j0; j < j1; j++) {
+                // Up
+                if (arr(i - 1, j) == fill_values<T>::FILLABLE) {
+                    locations.emplace(i - 1, j);
+                }
+            }
+        }
+
+        // Initiate scanlines below our current row
+        if (i < arr.getNumRows() - 1) {
+            for (j = j0; j < j1; j++) {
+                // Down
+                if (arr(i + 1, j) == fill_values<T>::FILLABLE) {
+                    locations.emplace(i + 1, j);
+                }
+            }
+        }
+    }
+}
+
+template<typename T>
+void
+FloodFill::flood(Matrix<T>& arr) const
+{
+
+    for (size_t i = 0; i < arr.getNumRows(); i++) {
+        for (size_t j = 0; j < arr.getNumCols(); j++) {
+            if (arr(i, j) == fill_values<T>::UNKNOWN) {
+                throw std::runtime_error("Cell with unknown position encountered.");
+            } else if (arr(i, j) == fill_values<T>::FILLABLE) {
+                // Cell position relative to polygon is unknown but can
+                // be determined from adjacent cells.
+                if (cellIsInside(i, j)) {
+                    floodFromPixel(arr, i, j, fill_values<T>::INTERIOR);
+                } else {
+                    floodFromPixel(arr, i, j, fill_values<T>::EXTERIOR);
+                }
+            }
+        }
+    }
+}
+
+}

src/operation/grid/FloodFill.cpp
@@ -1,0 +1,42 @@
+/**********************************************************************
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.osgeo.org
+ *
+ * Copyright (C) 2018-2025 ISciences, LLC
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation.
+ * See the COPYING file for more information.
+ *
+ **********************************************************************/
+
+#include <geos/operation/grid/FloodFill.h>
+#include <geos/operation/grid/Grid.h>
+#include <geos/algorithm/locate/IndexedPointInAreaLocator.h>
+#include <geos/geom/Location.h>
+
+using geos::geom::Geometry;
+
+namespace geos::operation::grid {
+
+FloodFill::FloodFill(const Geometry& g, const Grid<bounded_extent>& extent)
+  : m_extent{ extent }
+  , m_g{ g }
+  , m_loc{ nullptr }
+{
+  m_loc = std::make_unique<algorithm::locate::IndexedPointInAreaLocator>(m_g);
+}
+
+FloodFill::~FloodFill() = default;
+
+bool
+FloodFill::cellIsInside(size_t i, size_t j) const
+{
+    geom::CoordinateXY p(  m_extent.getColX(j), m_extent.getRowY(i) );
+
+    return m_loc->locate(&p) == geom::Location::INTERIOR;
+}
+
+}

src/operation/grid/Grid.cpp
@@ -1,0 +1,99 @@
+/**********************************************************************
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.osgeo.org
+ *
+ * Copyright (C) 2018-2025 ISciences, LLC
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation.
+ * See the COPYING file for more information.
+ *
+ **********************************************************************/
+
+#include <geos/operation/grid/Grid.h>
+
+using geos::geom::Envelope;
+
+namespace geos::operation::grid {
+
+template<>
+Envelope
+Grid<bounded_extent>::getCellEnvelope(size_t row, size_t col) const
+{
+    // The ternary clauses below are used to make sure that the cells along
+    // the right and bottom edges of our grid are slightly larger than m_dx,dy
+    // if needed to make sure that we capture our whole extent. This is necessary
+    // because xmin + nx*m_dx may be less than xmax because of floating point
+    // errors.
+    return {
+        xmin() + static_cast<double>(col) * dx(),
+        row == (getNumRows() - 1) ? ymin() : (ymax() - static_cast<double>(row + 1) * dy()),
+        col == (getNumCols() - 1) ? xmax() : (xmin() + static_cast<double>(col + 1) * dx()),
+        ymax() - static_cast<double>(row) * dy()
+    };
+}
+
+template<>
+Envelope
+Grid<infinite_extent>::getCellEnvelope(size_t row, size_t col) const
+{
+    double cell_xmin, cell_xmax, cell_ymin, cell_ymax;
+
+    constexpr double PADDED_CELL_SIZE = 1.0;
+
+    if (col == 0) {
+        cell_xmin = std::min(xmin() - PADDED_CELL_SIZE, m_domain.getMinX());
+    } else if (col == getNumCols() - 1) {
+        cell_xmin = xmax(); // because rightmost col of regular may have different width from others
+    } else {
+        cell_xmin = xmin() + static_cast<double>(col - 1) * dx();
+    }
+
+    switch (getNumCols() - col) {
+        case 1:
+            cell_xmax = std::max(xmax() + PADDED_CELL_SIZE, m_domain.getMaxX());
+            break;
+        case 2:
+            cell_xmax = xmax();
+            break;
+        default:
+            cell_xmax = xmin() + static_cast<double>(col) * dx();
+    }
+
+    if (row == 0) {
+        cell_ymax = std::max(xmax() + PADDED_CELL_SIZE, m_domain.getMaxX());
+    } else if (row == getNumRows() - 1) {
+        cell_ymax = ymin(); // because bottom row of regular may have different height from others
+    } else {
+        cell_ymax = ymax() - static_cast<double>(row - 1) * dy();
+    }
+
+    switch (getNumRows() - row) {
+        case 1:
+            cell_ymin = std::min(xmin() - PADDED_CELL_SIZE, m_domain.getMinY());
+            break;
+        case 2:
+            cell_ymin = ymin();
+            break;
+        default:
+            cell_ymin = ymax() - static_cast<double>(row) * dy();
+    }
+
+    return { cell_xmin, cell_xmax, cell_ymin, cell_ymax };
+}
+
+Grid<infinite_extent>
+make_infinite(const Grid<bounded_extent>& grid, const Envelope& domain)
+{
+    return { grid.getExtent(), grid.dx(), grid.dy() , domain };
+}
+
+Grid<bounded_extent>
+make_finite(const Grid<infinite_extent>& grid)
+{
+    return { grid.getExtent(), grid.dx(), grid.dy() };
+}
+
+}

src/operation/grid/Traversal.cpp
@@ -1,0 +1,106 @@
+/**********************************************************************
+ *
+ * GEOS - Geometry Engine Open Source
+ * http://geos.osgeo.org
+ *
+ * Copyright (C) 2018-2025 ISciences, LLC
+ *
+ * This is free software; you can redistribute and/or modify it under
+ * the terms of the GNU Lesser General Public Licence as published
+ * by the Free Software Foundation.
+ * See the COPYING file for more information.
+ *
+ **********************************************************************/
+
+#include <cstddef>
+#include <stdexcept>
+
+#include <geos/operation/grid/Traversal.h>
+
+using geos::geom::CoordinateXY;
+
+namespace geos::operation::grid {
+
+void
+Traversal::add(const CoordinateXY& c)
+{
+    m_coords.push_back(c);
+}
+
+bool
+Traversal::isEmpty() const
+{
+    return m_coords.empty();
+}
+
+void
+Traversal::enter(const CoordinateXY& c, Side s)
+{
+    if (!m_coords.empty()) {
+        throw std::runtime_error("Traversal already started");
+    }
+
+    add(c);
+    m_entry = s;
+}
+
+void
+Traversal::exit(const CoordinateXY& c, Side s)
+{
+    add(c);
+    m_exit = s;
+}
+
+bool
+Traversal::isClosedRing() const
+{
+    return m_coords.size() >= 3 && m_coords[0] == m_coords[m_coords.size() - 1];
+}
+
+bool
+Traversal::isEntered() const
+{
+    return m_entry != Side::NONE;
+}
+
+bool
+Traversal::isExited() const
+{
+    return m_exit != Side::NONE;
+}
+
+bool
+Traversal::hasMultipleUniqueCoordinates() const
+{
+    for (size_t i = 1; i < m_coords.size(); i++) {
+        if (m_coords[0] != m_coords[i]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool
+Traversal::isTraversed() const
+{
+    return isEntered() && isExited();
+}
+
+const CoordinateXY&
+Traversal::getLastCoordinate() const
+{
+    return m_coords.at(m_coords.size() - 1);
+}
+
+const CoordinateXY&
+Traversal::getExitCoordinate() const
+{
+    if (!isExited()) {
+        throw std::runtime_error("Can't get exit coordinate from incomplete traversal.");
+    }
+
+    return getLastCoordinate();
+}
+
+}