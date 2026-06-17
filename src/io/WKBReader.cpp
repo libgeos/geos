libgeos/geos

commit d271aaa9a1939fe5a51c5b5d05f1a3d547a2f900

Remove use of 'using namespace std'. References #915

src/io/WKBReader.cpp
@@ -40,7 +40,7 @@
 
 //#define DEBUG_WKB_READER 1
 
-using namespace std;
+
 using namespace geos::geom;
 
 namespace geos {
@@ -57,13 +57,13 @@ WKBReader::WKBReader()
     : WKBReader(*(GeometryFactory::getDefaultInstance()))
     {}
 
-ostream&
-WKBReader::printHEX(istream& is, ostream& os)
+std::ostream&
+WKBReader::printHEX(std::istream& is, std::ostream& os)
 {
     static const char hex[] = "0123456789ABCDEF";
 
     std::streampos pos = is.tellg(); // take note of input stream get pointer
-    is.seekg(0, ios::beg); // rewind input stream
+    is.seekg(0, std::ios::beg); // rewind input stream
 
     char each = 0;
     while(is.read(&each, 1)) {
@@ -131,22 +131,22 @@ ASCIIHexToUChar(char val)
 
 }  // namespace
 
-// Must be an even number of characters in the istream.
+// Must be an even number of characters in the std::istream.
 // Throws a ParseException if there are an odd number of characters.
 std::unique_ptr<Geometry>
-WKBReader::readHEX(istream& is)
+WKBReader::readHEX(std::istream& is)
 {
     // setup input/output stream
-    stringstream os(ios_base::binary | ios_base::in | ios_base::out);
+    std::stringstream os(std::ios_base::binary | std::ios_base::in | std::ios_base::out);
 
     while(true) {
         const int input_high = is.get();
-        if(input_high == char_traits<char>::eof()) {
+        if(input_high == std::char_traits<char>::eof()) {
             break;
         }
 
         const int input_low = is.get();
-        if(input_low == char_traits<char>::eof()) {
+        if(input_low == std::char_traits<char>::eof()) {
             throw ParseException("Premature end of HEX string");
         }
 
@@ -160,7 +160,7 @@ WKBReader::readHEX(istream& is)
             static_cast<char>((result_high << 4) + result_low);
 
 #if DEBUG_HEX_READER
-        cout << "HEX " << high << low << " -> DEC " << (int)value << endl;
+        std::size_t << "HEX " << high << low << " -> DEC " << (int)value << std::endl;
 #endif
         // write the value to the output stream
         os << value;
@@ -171,7 +171,7 @@ WKBReader::readHEX(istream& is)
 }
 
 std::unique_ptr<Geometry>
-WKBReader::read(istream& is)
+WKBReader::read(std::istream& is)
 {
     dis.setInStream(&is); // will default to machine endian
     return readGeometry();
@@ -184,7 +184,7 @@ WKBReader::readGeometry()
     unsigned char byteOrder = dis.readByte();
 
 #if DEBUG_WKB_READER
-    cout << "WKB byteOrder: " << (int)byteOrder << endl;
+    std::size_t << "WKB byteOrder: " << (int)byteOrder << std::endl;
 #endif
 
     // default is machine endian
@@ -207,7 +207,7 @@ WKBReader::readGeometry()
     int sfsqlHasM = (typeInt & 0x40000000) != 0;
 
 #if DEBUG_WKB_READER
-    cout << "WKB geometryType: " << geometryType << endl;
+    std::size_t << "WKB geometryType: " << geometryType << std::endl;
 #endif
 
     hasZ = sfsqlHasZ || isoHasZ;
@@ -223,17 +223,17 @@ WKBReader::readGeometry()
     }
 
 #if DEBUG_WKB_READER
-    cout << "WKB hasZ: " << hasZ << endl;
+    std::size_t << "WKB hasZ: " << hasZ << std::endl;
 #endif
 
 #if DEBUG_WKB_READER
-    cout << "WKB dimensions: " << inputDimension << endl;
+    std::size_t << "WKB dimensions: " << inputDimension << std::endl;
 #endif
 
     bool hasSRID = ((typeInt & 0x20000000) != 0);
 
 #if DEBUG_WKB_READER
-    cout << "WKB hasSRID: " << hasSRID << endl;
+    std::size_t << "WKB hasSRID: " << hasSRID << std::endl;
 #endif
 
     int SRID = 0;
@@ -266,7 +266,7 @@ WKBReader::readGeometry()
         result = readGeometryCollection();
         break;
     default:
-        stringstream err;
+        std::stringstream err;
         err << "Unknown WKB type " << geometryType;
         throw  ParseException(err.str());
     }
@@ -298,7 +298,7 @@ WKBReader::readLineString()
 {
     int size = dis.readInt();
 #if DEBUG_WKB_READER
-    cout << "WKB npoints: " << size << endl;
+    std::size_t << "WKB npoints: " << size << std::endl;
 #endif
     auto pts = readCoordinateSequence(size);
     return factory.createLineString(std::move(pts));
@@ -309,7 +309,7 @@ WKBReader::readLinearRing()
 {
     int size = dis.readInt();
 #if DEBUG_WKB_READER
-    cout << "WKB npoints: " << size << endl;
+    std::size_t << "WKB npoints: " << size << std::endl;
 #endif
     auto pts = readCoordinateSequence(size);
     return factory.createLinearRing(std::move(pts));
@@ -321,7 +321,7 @@ WKBReader::readPolygon()
     int numRings = dis.readInt();
 
 #if DEBUG_WKB_READER
-    cout << "WKB numRings: " << numRings << endl;
+    std::size_t << "WKB numRings: " << numRings << std::endl;
 #endif
 
     if(numRings == 0) {
@@ -353,7 +353,7 @@ WKBReader::readMultiPoint()
     for(int i = 0; i < numGeoms; i++) {
         geoms[i] = readGeometry();
         if(!dynamic_cast<Point*>(geoms[i].get())) {
-            stringstream err;
+            std::stringstream err;
             err << BAD_GEOM_TYPE_MSG << " MultiPoint";
             throw ParseException(err.str());
         }
@@ -371,7 +371,7 @@ WKBReader::readMultiLineString()
     for(int i = 0; i < numGeoms; i++) {
         geoms[i] = readGeometry();
         if(!dynamic_cast<LineString*>(geoms[i].get())) {
-            stringstream err;
+            std::stringstream err;
             err << BAD_GEOM_TYPE_MSG << " LineString";
             throw  ParseException(err.str());
         }
@@ -389,7 +389,7 @@ WKBReader::readMultiPolygon()
     for(int i = 0; i < numGeoms; i++) {
         geoms[i] = readGeometry();
         if(!dynamic_cast<Polygon*>(geoms[i].get())) {
-            stringstream err;
+            std::stringstream err;
             err << BAD_GEOM_TYPE_MSG << " Polygon";
             throw ParseException(err.str());
         }
@@ -445,7 +445,7 @@ WKBReader::readCoordinate()
         }
     }
 #if DEBUG_WKB_READER
-    cout << "WKB coordinate: " << ordValues[0] << "," << ordValues[1] << endl;
+    std::size_t << "WKB coordinate: " << ordValues[0] << "," << ordValues[1] << std::endl;
 #endif
 }
 

src/io/WKBWriter.cpp
@@ -39,7 +39,7 @@
 
 #undef DEBUG_WKB_WRITER
 
-using namespace std;
+
 using namespace geos::geom;
 
 namespace geos {
@@ -67,10 +67,10 @@ WKBWriter::setOutputDimension(uint8_t dims)
 }
 
 void
-WKBWriter::writeHEX(const Geometry& g, ostream& os)
+WKBWriter::writeHEX(const Geometry& g, std::ostream& os)
 {
     // setup input/output stream
-    stringstream stream;
+    std::stringstream stream;
 
     // write the geometry in wkb format
     this->write(g, stream);
@@ -80,7 +80,7 @@ WKBWriter::writeHEX(const Geometry& g, ostream& os)
 }
 
 void
-WKBWriter::write(const Geometry& g, ostream& os)
+WKBWriter::write(const Geometry& g, std::ostream& os)
 {
     outputDimension = defaultOutputDimension;
     if(outputDimension > g.getCoordinateDimension()) {
@@ -301,11 +301,11 @@ WKBWriter::writeCoordinateSequence(const CoordinateSequence& cs,
 }
 
 void
-WKBWriter::writeCoordinate(const CoordinateSequence& cs, size_t idx,
+WKBWriter::writeCoordinate(const CoordinateSequence& cs, std::size_t idx,
                            bool is3d)
 {
 #if DEBUG_WKB_WRITER
-    cout << "writeCoordinate: X:" << cs.getX(idx) << " Y:" << cs.getY(idx) << endl;
+    std::size_t << "writeCoordinate: X:" << cs.getX(idx) << " Y:" << cs.getY(idx) << std::endl;
 #endif
     assert(outStream);
 

src/operation/overlay/snap/GeometrySnapper.cpp
@@ -34,7 +34,7 @@
 #include <memory>
 #include <algorithm>
 
-//using namespace std;
+//
 using namespace geos::geom;
 
 namespace geos {
@@ -62,10 +62,10 @@ private:
         std::vector<Coordinate> coords;
         srcPts->toVector(coords);
         LineStringSnapper snapper(coords, snapTol);
-        unique_ptr<Coordinate::Vect> newPts = snapper.snapTo(snapPts);
+        std::unique_ptr<Coordinate::Vect> newPts = snapper.snapTo(snapPts);
 
         const CoordinateSequenceFactory* cfact = factory->getCoordinateSequenceFactory();
-        return unique_ptr<CoordinateSequence>(cfact->create(newPts.release()));
+        return std::unique_ptr<CoordinateSequence>(cfact->create(newPts.release()));
     }
 
 public:
@@ -111,11 +111,11 @@ GeometrySnapper::snapTo(const geom::Geometry& g, double snapTolerance)
     using geom::util::GeometryTransformer;
 
     // Get snap points
-    unique_ptr<Coordinate::ConstVect> snapPts = extractTargetCoordinates(g);
+    std::unique_ptr<Coordinate::ConstVect> snapPts = extractTargetCoordinates(g);
 
     // Apply a SnapTransformer to source geometry
     // (we need a pointer for dynamic polymorphism)
-    unique_ptr<GeometryTransformer> snapTrans(new SnapTransformer(snapTolerance, *snapPts));
+    std::unique_ptr<GeometryTransformer> snapTrans(new SnapTransformer(snapTolerance, *snapPts));
     return snapTrans->transform(&srcGeom);
 }
 
@@ -128,11 +128,11 @@ GeometrySnapper::snapToSelf(double snapTolerance, bool cleanResult)
     using geom::util::GeometryTransformer;
 
     // Get snap points
-    unique_ptr<Coordinate::ConstVect> snapPts = extractTargetCoordinates(srcGeom);
+    std::unique_ptr<Coordinate::ConstVect> snapPts = extractTargetCoordinates(srcGeom);
 
     // Apply a SnapTransformer to source geometry
     // (we need a pointer for dynamic polymorphism)
-    unique_ptr<GeometryTransformer> snapTrans(new SnapTransformer(snapTolerance, *snapPts));
+    std::unique_ptr<GeometryTransformer> snapTrans(new SnapTransformer(snapTolerance, *snapPts));
 
     GeomPtr result = snapTrans->transform(&srcGeom);
 
@@ -207,8 +207,8 @@ GeometrySnapper::snap(const geom::Geometry& g0,
     GeometrySnapper snapper1(g1);
     snapGeom.second = snapper1.snapTo(*snapGeom.first, snapTolerance);
 
-//	cout << *snapGeom.first << endl;
-//	cout << *snapGeom.second << endl;
+//	std::size_t << *snapGeom.first << std::endl;
+//	std::size_t << *snapGeom.second << std::endl;
 
 }
 

commit a10120964ad8c8e772f35898cfe65491d08610fd

Fix `size_t <<` debug prints
Bug introduced in https://github.com/libgeos/geos/commit/d271aaa9a1939fe5a51c5b5d05f1a3d547a2f900

src/io/WKBReader.cpp
@@ -46,6 +46,9 @@
 
 //#define DEBUG_WKB_READER 1
 
+#if DEBUG_WKB_READER
+#include <iostream>
+#endif
 
 using namespace geos::geom;
 
@@ -174,7 +177,7 @@ WKBReader::readHEX(std::istream& is)
             static_cast<unsigned char>((result_high << 4) + result_low);
 
 #if DEBUG_HEX_READER
-        std::size_t << "HEX " << high << low << " -> DEC " << (int)value << std::endl;
+        std::cout << "HEX " << high << low << " -> DEC " << (int)value << std::endl;
 #endif
         // write the value to the output stream
         os << value;
@@ -272,7 +275,7 @@ WKBReader::readGeometry()
     unsigned char byteOrder = dis.readByte();
 
 #if DEBUG_WKB_READER
-    std::size_t << "WKB byteOrder: " << (int)byteOrder << std::endl;
+    std::cout << "WKB byteOrder: " << (int)byteOrder << std::endl;
 #endif
 
     // default is machine endian
@@ -295,7 +298,7 @@ WKBReader::readGeometry()
     int sfsqlHasM = (typeInt & 0x40000000) != 0;
 
 #if DEBUG_WKB_READER
-    std::size_t << "WKB geometryType: " << geometryType << std::endl;
+    std::cout << "WKB geometryType: " << geometryType << std::endl;
 #endif
 
     hasZ = sfsqlHasZ || isoHasZ;
@@ -311,17 +314,17 @@ WKBReader::readGeometry()
     }
 
 #if DEBUG_WKB_READER
-    std::size_t << "WKB hasZ: " << hasZ << std::endl;
+    std::cout << "WKB hasZ: " << hasZ << std::endl;
 #endif
 
 #if DEBUG_WKB_READER
-    std::size_t << "WKB dimensions: " << inputDimension << std::endl;
+    std::cout << "WKB dimensions: " << inputDimension << std::endl;
 #endif
 
     bool hasSRID = ((typeInt & 0x20000000) != 0);
 
 #if DEBUG_WKB_READER
-    std::size_t << "WKB hasSRID: " << hasSRID << std::endl;
+    std::cout << "WKB hasSRID: " << hasSRID << std::endl;
 #endif
 
     int SRID = 0;
@@ -398,7 +401,7 @@ WKBReader::readLineString()
     uint32_t size = dis.readUnsigned();
     minMemSize(GEOS_LINESTRING, size);
 #if DEBUG_WKB_READER
-    std::size_t << "WKB npoints: " << size << std::endl;
+    std::cout << "WKB npoints: " << size << std::endl;
 #endif
     auto pts = readCoordinateSequence(size);
     return factory.createLineString(std::move(pts));
@@ -410,7 +413,7 @@ WKBReader::readLinearRing()
     uint32_t size = dis.readUnsigned();
     minMemSize(GEOS_LINEARRING, size);
 #if DEBUG_WKB_READER
-    std::size_t << "WKB npoints: " << size << std::endl;
+    std::cout << "WKB npoints: " << size << std::endl;
 #endif
     auto pts = readCoordinateSequence(size);
     // Replace unclosed ring with closed
@@ -451,7 +454,7 @@ WKBReader::readPolygon()
     minMemSize(GEOS_POLYGON, numRings);
 
 #if DEBUG_WKB_READER
-    std::size_t << "WKB numRings: " << numRings << std::endl;
+    std::cout << "WKB numRings: " << numRings << std::endl;
 #endif
 
     std::unique_ptr<LinearRing> shell;
@@ -481,7 +484,7 @@ WKBReader::readCurvePolygon()
     minMemSize(GEOS_POLYGON, numRings);
 
 #if DEBUG_WKB_READER
-    std::size_t << "WKB numRings: " << numRings << std::endl;
+    std::cout << "WKB numRings: " << numRings << std::endl;
 #endif
 
     if (numRings == 0) {
@@ -624,7 +627,7 @@ WKBReader::readCoordinate()
         }
     }
 #if DEBUG_WKB_READER
-    std::size_t << "WKB coordinate: " << ordValues[0] << "," << ordValues[1] << std::endl;
+    std::cout << "WKB coordinate: " << ordValues[0] << "," << ordValues[1] << std::endl;
 #endif
 }
 

src/io/WKBWriter.cpp
@@ -42,6 +42,10 @@
 
 #undef DEBUG_WKB_WRITER
 
+#if DEBUG_WKB_READER
+#include <iostream>
+#endif
+
 
 using namespace geos::geom;
 
@@ -380,7 +384,7 @@ void
 WKBWriter::writeCoordinate(const CoordinateSequence& cs, std::size_t idx)
 {
 #if DEBUG_WKB_WRITER
-    std::size_t << "writeCoordinate: X:" << cs.getX(idx) << " Y:" << cs.getY(idx) << std::endl;
+    std::cout << "writeCoordinate: X:" << cs.getX(idx) << " Y:" << cs.getY(idx) << std::endl;
 #endif
     assert(outStream);
 
@@ -442,4 +446,3 @@ WKBWriter::getWkbType(const Geometry& g) {
 
 } // namespace geos.io
 } // namespace geos
-

src/operation/overlay/snap/GeometrySnapper.cpp
@@ -209,8 +209,8 @@ GeometrySnapper::snap(const geom::Geometry& g0,
     GeometrySnapper snapper1(g1);
     snapGeom.second = snapper1.snapTo(*snapGeom.first, snapTolerance);
 
-//	std::size_t << *snapGeom.first << std::endl;
-//	std::size_t << *snapGeom.second << std::endl;
+//	std::cout << *snapGeom.first << std::endl;
+//	std::cout << *snapGeom.second << std::endl;
 
 }
 
@@ -228,4 +228,3 @@ GeometrySnapper::snapToSelf(const geom::Geometry& g,
 } // namespace geos.operation.overlay
 } // namespace geos.operation
 } // namespace geos
-